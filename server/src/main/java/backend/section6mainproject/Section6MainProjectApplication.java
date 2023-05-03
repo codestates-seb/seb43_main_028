07782959ba3e9ddc73cca0ba4085b0fc56374305
kim-ksp7331 <ksp7331@gmail.com>
2023-05-03 14:34:56 +0900

    Chore: 백엔드 기본환경 설정 (#3)

---
+++ server/src/main/java/backend/section6mainproject/Section6MainProjectApplication.java
@@ -0,0 +1,13 @@
+package backend.section6mainproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section6MainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section6MainProjectApplication.class, args);
+	}
+
+}
