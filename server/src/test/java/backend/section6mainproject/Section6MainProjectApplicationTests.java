78923e94cdbd68f0c91fc77499debfe071800606
kim-ksp7331 <ksp7331@gmail.com>
2023-05-03 14:34:56 +0900

    Chore: 백엔드 기본환경 설정 (#3)

---
+++ server/src/test/java/backend/section6mainproject/Section6MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.section6mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section6MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
