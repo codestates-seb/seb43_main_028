64ae7640c9ee6eda1d0cf796db6ddf7813bc58d7
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-04 20:30:19 +0900

    chore: Vite - React - TypeScript -Eslint - prettier 설정 (#11)
    - vite create로 react-ts 프로젝트 생성
    - eslint의 airbnb rule 적용
    - eslint-prettier 설정 결합 : prettier의 rule을 eslint에서 잡을 수 있게 변경

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  root: true,
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-shadow': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['htmlFor'],
+      },
+    ],
+  },
+}
