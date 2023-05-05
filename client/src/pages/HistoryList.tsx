8adecaea84d60a00e9ee0b48698b0e6122f70bb6
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-05 18:34:47 +0900

    [FE] 기본 라우터 설정 & 폴더 구조 잡기 #21 (#22)
    * Feat: 사용할 page 파일들 생성 (#21)
    
    * Feat: router 생성 & 적용 (#21)
    
    * Feat: 페이지 파일과 대응하는 컴포넌트 폴더 생성 (#21)
    
    * Feat: 폴더 생성 - apis, assets, hooks, utils (#21)
    
    * Fix : 오타 수정 (#21)

---
+++ client/src/pages/HistoryList.tsx
@@ -0,0 +1,3 @@
+export default function HistoryList() {
+  return <div>HistoryList</div>
+}
---
+++ client/src/pages/index.ts
@@ -0,0 +1,23 @@
+import Home from './Home'
+import Feed from './Feed'
+import MyPage from './MyPage'
+import AfterWalk from './AfterWalk'
+import OnWalk from './OnWalk'
+import HistoryDetail from './HistoryDetail'
+import HistoryList from './HistoryList'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+import NotFound from './NotFound'
+
+export {
+  Home,
+  Feed,
+  MyPage,
+  AfterWalk,
+  OnWalk,
+  HistoryDetail,
+  HistoryList,
+  SignIn,
+  SignUp,
+  NotFound,
+}
