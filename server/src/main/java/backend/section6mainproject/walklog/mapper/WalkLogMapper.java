6342e7b6b052b9b3c5834f55346dc5289665279f
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-05-05 11:40:37 +0900

    Feat: WalkLog post 기능 구현 및 테스트 작성 (#7) (#17)
    * Feat: WalkLog post 기능 구현 및 테스트 작성 (#7)
    
    * Feat: WalkLog post 기능 구현 및 테스트 작성 (#7)
    
    * Feat: WalkLog post 기능 구현 및 테스트 작성 (#7)
    
    * Feat: WalkLog post 기능 구현 및 테스트 작성 (#7)

---
+++ server/src/main/java/backend/section6mainproject/walklog/mapper/WalkLogMapper.java
@@ -0,0 +1,10 @@
+package backend.section6mainproject.walklog.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface WalkLogMapper {
+
+
+}
