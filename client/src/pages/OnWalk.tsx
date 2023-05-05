15aa7f1fffb2e5f2542279385ab7ce2c77be3bd3
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-05 17:59:40 +0900

    Feat: 사용할 page 파일들 생성 (#21)

---
+++ client/src/pages/OnWalk.tsx
@@ -0,0 +1,3 @@
+export default function OnWalk() {
+  return <div>OnWalk</div>
+}
---
+++ client/src/pages/index.ts
@@ -0,0 +1,23 @@
+import Home from './Home'
+import Feed from './Feed'
+import Mypage from './Mypage'
+import AfterWalk from './AfterWalk'
+import OnWalk from './OnWalk'
+import HistoryDetail from './HistoryDetail'
+import HistoryList from './HistoryList'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+import NotFound from './NotFound'
+
+export {
+  Home,
+  Feed,
+  Mypage,
+  AfterWalk,
+  OnWalk,
+  HistoryDetail,
+  HistoryList,
+  SignIn,
+  SignUp,
+  NotFound,
+}
