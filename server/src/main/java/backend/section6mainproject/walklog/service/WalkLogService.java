8bf4970edea743d0bbe1e0bbfb5fa2ec5a4fb599
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-05-05 11:17:14 +0900

    Feat: WalkLog post 기능 구현 및 테스트 작성 (#7)

--- server/src/main/java/backend/section6mainproject/walklog/service/WalkLogService.java
+++ server/src/main/java/backend/section6mainproject/walklog/service/WalkLogService.java
@@ -6,11 +6,13 @@ import backend.section6mainproject.walklog.entity.WalkLog;
 import backend.section6mainproject.walklog.repository.WalkLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class WalkLogService {
 
     private final WalkLogRepository walkLogRepository;
