634fb2aee972ccd144084bd6f91f9de36f625984
iberis2 <snow9402@gmail.com>
2023-05-08 17:28:46 +0900

    Feat: 순간기록-이미지Input UI 1차 완성 (#27)
    이미지 preview UI 완성,
    카메라 아이콘, 모달 컴포넌츠 받아서 카메라 및 앨범 에서 이미지 선택 기능 연결 필요

--- client/src/components/OnWalk/ImgInput.tsx
+++ client/src/components/OnWalk/ImgInput.tsx
@@ -1,10 +1,54 @@
+import { useState, useRef, useEffect } from 'react'
 import Camera from './Camera'
+import styles from './ImgInput.module.scss'
 
 export default function ImgInput() {
+  const [preview, setPreview] = useState<string | undefined>('')
+  const [imgFile, setImgFile] = useState<File | undefined>()
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImgFile(event.target.files?.[0])
+  }
+
+  const handleClear = () => {
+    if (!inputRef.current) return
+    inputRef.current.value = ''
+    setImgFile(undefined)
+  }
+
+  useEffect(() => {
+    if (!imgFile) return setPreview('')
+
+    const nextPreview = URL.createObjectURL(imgFile)
+    setPreview(nextPreview)
+
+    return () => {
+      URL.revokeObjectURL(nextPreview)
+      setPreview('')
+    }
+  }, [imgFile])
+
   return (
-    <div>
-      imageInput
-      <Camera />
+    <div className={styles.container}>
+      <div className={styles.previewBox}>
+        {imgFile ? <img src={preview} alt='이미지 미리보기' /> : 'No Image'}
+      </div>
+      {imgFile && (
+        <button type='button' onClick={handleClear}>
+          X
+        </button>
+      )}
+      <button type='button' className={styles.cameraBtn}>
+        <Camera />
+        <input
+          id='photo'
+          type='file'
+          ref={inputRef}
+          onChange={handleChange}
+          accept='image/png, image/jpeg'
+        />
+      </button>
     </div>
   )
 }
