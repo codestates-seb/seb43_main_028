fd2617bab7f815af401e4f0345d700be4dcf8435
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-05-08 13:26:37 +0900

    18 be feat walklog get기능 구현 (#30)
    * Feat: WalkLog Get 기능 구현 (#30)

--- server/src/main/java/backend/section6mainproject/walklog/service/WalkLogService.java
+++ server/src/main/java/backend/section6mainproject/walklog/service/WalkLogService.java
@@ -1,46 +1,13 @@
 package backend.section6mainproject.walklog.service;
 
-import backend.section6mainproject.member.entity.Member;
-import backend.section6mainproject.member.service.MemberService;
 import backend.section6mainproject.walklog.entity.WalkLog;
-import backend.section6mainproject.walklog.repository.WalkLogRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+public interface WalkLogService {
+    WalkLog createWalkLog(Long memberId);
 
-@Service
-@RequiredArgsConstructor
-@Transactional
-public class WalkLogService {
+    WalkLog updateWalkLog(WalkLog walkLog);
 
-    private final WalkLogRepository walkLogRepository;
-    private final MemberService memberService;
+    WalkLog findWalkLog(Long walkLogId);
+    WalkLog findVerifiedWalkLog(Long walkLogId);
 
-    public WalkLog createWalkLog(Long memberId){
-        WalkLog walkLog = new WalkLog();
-        Member findVerifiedMember = memberService.findVerifiedMember(memberId);
-        walkLog.setMember(findVerifiedMember);
-        return walkLogRepository.save(walkLog);
-    }
-    public WalkLog updateWalkLog(WalkLog walkLog){
-        WalkLog findWalkLog = findVerifiedWalkLog(walkLog.getWalkLogId());
-
-        Optional.ofNullable(walkLog.getWalkLogPublicSetting())
-                .ifPresent(walkLogPublicSetting -> findWalkLog.setWalkLogPublicSetting(walkLogPublicSetting));
-        Optional.ofNullable(walkLog.getMessage())
-                .ifPresent(message -> findWalkLog.setMessage(message));
-
-        return walkLogRepository.save(findWalkLog);
-    }
-
-    public WalkLog findVerifiedWalkLog(long walkLogId) {
-        Optional<WalkLog> findWalkLogById = walkLogRepository.findById(walkLogId);
-
-        WalkLog walkLog =
-                findWalkLogById.orElseThrow(() ->
-                        new RuntimeException("WalkLog를 찾을 수 없습니다")); //잘못된 문구 수정
-        return walkLog;
-    }
 }
