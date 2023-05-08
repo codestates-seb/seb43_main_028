36647ef232fa214e30d6c2542e7000dcbe4e1b10
hajongon <hajongon@gmail.com>
2023-05-08 17:30:53 +0900

    Feat: google map 렌더링 (#66)
    - @react-google-maps/api 를 이용한 google map 렌더링
    - VITE_GOOGLE_MAPS_API_KEY 환경변수 사용
    - 임시적으로 지도 center 설정(서울시청 좌표)

--- client/src/components/common/map.tsx
+++ client/src/components/common/map.tsx
@@ -1,5 +1,28 @@
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import { useState, useEffect } from 'react'
+
+const containerStyle = {
+  width: '340px',
+  height: '340px',
+}
+
 function Map() {
-  return <div>Map Test</div>
+  const [apiKey, setApiKey] = useState('')
+
+  useEffect(() => {
+    const { VITE_GOOGLE_MAPS_API_KEY } = import.meta.env
+    setApiKey(VITE_GOOGLE_MAPS_API_KEY)
+  }, [])
+
+  return (
+    <LoadScript googleMapsApiKey={apiKey} onLoad={() => console.log('Loaded!')}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        zoom={14}
+        center={{ lat: 37.5662952, lng: 126.9779451 }}
+      />
+    </LoadScript>
+  )
 }
 
 export default Map
