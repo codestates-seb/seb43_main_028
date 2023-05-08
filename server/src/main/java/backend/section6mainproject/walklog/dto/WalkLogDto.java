c37e4e8ff6aa22f8a05f6e0395a15828c0d514c6
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-05-08 11:34:33 +0900

    Feat: WalkLog petch 기능 구현 및 테스트 작성 (#18) (#25)
    * Feat,Test,Formatting: WalkLog petch 기능 구현 및 테스트 작성,MemberMapper 메서드명 카멜케이스변경 (#7)
    
    * Feat,Test,Formatting: WalkLog petch 기능 구현 및 테스트 작성,MemberMapper 메서드명 카멜케이스변경 (#7)
    
    * Feat,Test,Formatting: WalkLog petch 기능 구현 및 테스트 작성,MemberMapper 메서드명 카멜케이스변경 (#7)

--- server/src/main/java/backend/section6mainproject/walklog/dto/WalkLogDto.java
+++ server/src/main/java/backend/section6mainproject/walklog/dto/WalkLogDto.java
@@ -1,14 +1,78 @@
 package backend.section6mainproject.walklog.dto;
 
 
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static backend.section6mainproject.walklog.entity.WalkLog.*;
 
 public class WalkLogDto {
     @Getter
     @Setter
+    @NoArgsConstructor
     public static class Post {
         private Long memberId;
     }
 
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Patch{
+        private String message;
+        private WalkLogPublicSetting walkLogPublicSetting;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class Response{
+
+        private Long walkLogId;
+        private LocalDateTime endTime;
+
+        private String message;
+        private WalkLogDto.MemberResponse member;
+
+        private WalkLogPublicSetting walkLogPublicSetting;
+        private List<CoordinateResponse> coordinates;
+        private List<ContentResponse> walkLogContents;
+    }
+//패스워드와 나머지 객체들은 반환할 필요가 없어서 제거하고 Dto생성
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class MemberResponse {
+        private Long memberId;
+        private String nickname;
+
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class CoordinateResponse {
+
+        //순환참조 방지를 위해 Dto를 따로 생성했습니다.
+        private Long coordinateId;
+        private LocalDateTime createdAt;
+        private Double lat;
+        private Double lng;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class ContentResponse {
+        //순환참조 방지를 위해 Dto를 따로 만들었습니다.
+
+        private Long walkLogContentId;
+
+        private String text;
+    }
 }
--- server/src/test/java/backend/section6mainproject/walklog/controller/WalkLogControllerTest.java
+++ server/src/test/java/backend/section6mainproject/walklog/controller/WalkLogControllerTest.java
@@ -1,12 +1,20 @@
 package backend.section6mainproject.walklog.controller;
 
+import backend.section6mainproject.member.entity.Member;
 import backend.section6mainproject.walklog.dto.WalkLogDto;
 
+import backend.section6mainproject.walklog.entity.WalkLog;
+import backend.section6mainproject.walklog.mapper.WalkLogMapper;
+import backend.section6mainproject.walklog.repository.WalkLogRepository;
+import backend.section6mainproject.walklog.service.WalkLogService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -14,9 +22,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 
 @Transactional
@@ -26,6 +38,11 @@ public class WalkLogControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private WalkLogService walkLogService;
+
+
+
 
 
     @Test
@@ -35,6 +52,22 @@ public class WalkLogControllerTest {
         post.setMemberId(1L);
         String content = objectMapper.writeValueAsString(post);
 
+        Long memberId = 1L;
+        Member member = new Member();
+        member.setMemberId(memberId);
+        member.setEmail("admin1@gmail.com");
+        member.setPassword("12345");
+        member.setNickname("거터볼래1");
+        member.setIntroduction("안녕하세요1");
+        //온전한 WalkLog 객체
+        Long walkLogId = 1L;
+        WalkLog walkLog = new WalkLog();
+        walkLog.setMember(member);
+        walkLog.setWalkLogId(walkLogId);
+        walkLog.setMessage("안녕하십니까");
+
+
+        given(walkLogService.createWalkLog(Mockito.anyLong())).willReturn(walkLog);
 
         // when
         ResultActions actions =
@@ -49,6 +82,56 @@ public class WalkLogControllerTest {
         actions
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", is(startsWith("/walk-logs"))));
+    }
+
+    @Test
+    void patchWalkLogTest() throws Exception {
+        //given
+        ObjectMapper objectMapper = new ObjectMapper();
+        //온전한 WalkLog객체를 위해 온전한 Member 객체가 1개 필요
+        Long memberId = 1L;
+        Member member = new Member();
+        member.setMemberId(memberId);
+        member.setEmail("admin1@gmail.com");
+        member.setPassword("12345");
+        member.setNickname("거터볼래1");
+        member.setIntroduction("안녕하세요1");
+        //온전한 WalkLog 객체
+        Long walkLogId = 1L;
+        WalkLog walkLog = new WalkLog();
+        walkLog.setMember(member);
+        walkLog.setWalkLogId(walkLogId);
+        walkLog.setMessage("안녕하십니까");
+
+        //WalkLogDto.Patch 객체가 1개 필요
+        WalkLogDto.Patch patchWalkLogDto = new WalkLogDto.Patch();
+        patchWalkLogDto.setMessage("안녕하십니끄아악!");
+        patchWalkLogDto.setWalkLogPublicSetting(WalkLog.WalkLogPublicSetting.PUBLIC);
+        //updated된 WalkLog 객체
+        WalkLog updatedWalkLog = new WalkLog();
+        updatedWalkLog.setMember(member);
+        updatedWalkLog.setWalkLogId(walkLogId);
+        updatedWalkLog.setMessage("안녕하십니끄아악!");
+        updatedWalkLog.setWalkLogPublicSetting(WalkLog.WalkLogPublicSetting.PUBLIC);
+        //Json 데이터 생성
+        String jsonPatchWalkLogDto = objectMapper.writeValueAsString(patchWalkLogDto);
+        //walkLogService.updateWalkLog메서드 로직 Mock수행
+        given(walkLogService.updateWalkLog(Mockito.any(WalkLog.class))).willReturn(updatedWalkLog);
+        //when
+        //patchWalkLog메서드를 수행 했을 때
+        ResultActions perform = mockMvc.perform(
+                patch("/walk-logs/" + walkLogId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(jsonPatchWalkLogDto));
+
+        //then
+        perform
+        //예상되는 결과는 status가 isok
+                .andExpect(status().isOk())
+        //WalkLogDto.Patch객체로 전달받은 데이터들이 변경되었는지확인
+                .andExpect(jsonPath("$.message").value(updatedWalkLog.getMessage()));
+//                .andExpect(jsonPath("$.walkLogPublicSetting").value(response.getWalkLogPublicSetting())); enum이라 에러가 발생하는듯함
     }
 
 }
