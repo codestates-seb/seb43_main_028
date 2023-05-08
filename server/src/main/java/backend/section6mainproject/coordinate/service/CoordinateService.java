f16823a14a3480c6abb725a471dca25123b5d724
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-05-08 11:35:48 +0900

    Feat: 웹소켓을 이용한 좌표 전송 기능 구현 (#9) (#23)
    * Feat: 웹소켓을 이용한 좌표 전송 기능 구현 (#9)
    
    * Fix: 웹소켓 응답 메시지에 coordinateId 추가 (#9)
    
    * Fix: walkLogId 자동 반영 (#9)

---
+++ server/src/main/java/backend/section6mainproject/coordinate/service/CoordinateService.java
@@ -0,0 +1,7 @@
+package backend.section6mainproject.coordinate.service;
+
+import backend.section6mainproject.coordinate.entity.Coordinate;
+
+public interface CoordinateService {
+    Coordinate createCoordinate(Coordinate coordinate);
+}
