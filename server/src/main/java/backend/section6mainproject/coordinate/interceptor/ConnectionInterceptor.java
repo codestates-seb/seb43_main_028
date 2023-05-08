c07a7aab40c309e9f3a0b26cabe0df494b421183
Leejh <97214060+NOTE111@users.noreply.github.com>
2023-05-08 15:01:00 +0900

    Feat: Member Patch (#33) (#34)

--- server/src/main/java/backend/section6mainproject/coordinate/interceptor/ConnectionInterceptor.java
+++ server/src/main/java/backend/section6mainproject/coordinate/interceptor/ConnectionInterceptor.java
@@ -1,13 +1,12 @@
 package backend.section6mainproject.coordinate.interceptor;
 
 import backend.section6mainproject.member.entity.Member;
-import backend.section6mainproject.member.service.MemberService;
+import backend.section6mainproject.member.service.MemberServiceImpl;
 import backend.section6mainproject.walklog.entity.WalkLog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
@@ -17,13 +16,13 @@ import java.util.Map;
 @Component
 @RequiredArgsConstructor
 public class ConnectionInterceptor implements HandshakeInterceptor {
-    private final MemberService memberService;
+    private final MemberServiceImpl memberServiceImpl;
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
         // 인증 로직 구현 후, 토큰에서 memberId를 가져오게 할 예정이다.
         Long memberId = 1L;
-        Member findMember = memberService.findVerifiedMember(memberId);
+        Member findMember = memberServiceImpl.findVerifiedMember(memberId);
         List<WalkLog> walkLogs = findMember.getWalkLogs();
 
         // 기록 상태를 나타내는 상태값 구현후 변경 예정, 임시로 마지막 walkLog의 Id를 가져오게 했다.
--- server/src/main/java/backend/section6mainproject/member/service/MemberService.java
+++ server/src/main/java/backend/section6mainproject/member/service/MemberService.java
@@ -1,46 +1,11 @@
 package backend.section6mainproject.member.service;
 
 import backend.section6mainproject.member.entity.Member;
-import backend.section6mainproject.member.repository.MemberRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-@Transactional
-@Service
-public class MemberService {
-    private final MemberRepository memberRepository;
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+public interface MemberService {
+    Member createMember(Member member);
 
-    public Member createMember(Member member) {
-        verifyExistsEmail(member.getEmail());
+    Member updateMember(Member member);
 
-        //추후 패스워드 암호화 및 User Role 관련해서 작업필요
-
-        Member savedMember = memberRepository.save(member);
-        return savedMember;
-    }
-
-    private void verifyExistsEmail(String email) {
-       Optional<Member> findMember = memberRepository.findByEmail(email);
-       if(findMember.isPresent()) {
-           throw new RuntimeException("이미 존재하는 회원의 아이디입니다.");
-       }
-    }
-
-    /** 회원이 리포지토리에 존재하는지 여부를 검증하고, 회원을 반환하는 메서드.
-     * <p>Member 엔티티의 walkLogs 필드는 fetchType이 Lazy로 설정되어 있으므로, 값을 실제로 조회할 때 데이터를 호출하게 되는데,
-     *  ConnectionInterceptor가 영속성 컨텍스트 밖에 있는것으로 보여서 이 메서드에서 값을 호출했다.
-     */
-    public Member findVerifiedMember(long memberId) {
-        Optional<Member> optionalMember =
-                memberRepository.findById(memberId);
-        Member findMember =
-                optionalMember.orElseThrow(() ->
-                        new RuntimeException("회원이 존재하지 않습니다"));
-        findMember.getWalkLogs().stream().forEach(walkLog -> walkLog.getWalkLogId());
-        return findMember;
-    }
+    Member findVerifiedMember(long memberId);
 }
