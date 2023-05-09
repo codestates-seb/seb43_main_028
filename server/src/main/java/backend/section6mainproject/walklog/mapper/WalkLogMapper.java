f397f0313232305fdbff062e913281ef7a8847b6
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-05-09 11:24:36 +0900

    Test: WalkLog get 기능 구현 (#31) (#40)
    * Test: WalkLog get 기능 구현 (#31)
    
    * Test: WalkLog get 기능 구현 (#31)
    
    * Test: WalkLog get 기능 구현 (#31)
    
    * Test: WalkLog get 기능 구현 (#31)
    
    ---------
    
    Co-authored-by: kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>

--- server/src/main/java/backend/section6mainproject/walklog/mapper/WalkLogMapper.java
+++ server/src/main/java/backend/section6mainproject/walklog/mapper/WalkLogMapper.java
@@ -2,22 +2,23 @@ package backend.section6mainproject.walklog.mapper;
 
 import backend.section6mainproject.content.entity.WalkLogContent;
 import backend.section6mainproject.coordinate.entity.Coordinate;
+import backend.section6mainproject.coordinate.mapper.CoordinateMapper;
 import backend.section6mainproject.member.entity.Member;
 import backend.section6mainproject.walklog.dto.WalkLogDTO;
 import backend.section6mainproject.walklog.entity.WalkLog;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",uses = {CoordinateMapper.class})
 public interface WalkLogMapper {
     WalkLog walkLogPatchDtoToWalkLog(WalkLogDTO.Patch walkLogPatchDto);
 
+    @Mapping(source = "member.memberId",target = "memberId")
+    @Mapping(source = "member.nickname",target = "nickname")
+    @Mapping(target = "coordinates",qualifiedByName = "coordinatesToCoordinateSubDTOs")
     WalkLogDTO.Response walkLogToWalkLogResponseDto(WalkLog walkLog);
 
-
-    WalkLogDTO.MemberResponse memberToWalkLogMemberResponseDto(Member member);
-
     WalkLogDTO.ContentResponse walkLogContentToWalkLogContentResponseDto(WalkLogContent walkLogContent);
-    WalkLogDTO.CoordinateResponse coordinateToWalkLogCoordinateResponseDto(Coordinate coordinate);
 
 }
