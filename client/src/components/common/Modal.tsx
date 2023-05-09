20300e73462204f4b4ac7701c513b259077024db
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-09 14:54:28 +0900

    [FE] Feat: Modal Window 컴포넌트 구현 (#55)
    * Feat: Modal 컴포넌트 구현 (#39)
    
    - Modal 컴포넌트가 상속받는 Props의 타입 정의
    - Modal 백그라운드 클릭시 실행될 함수 정의
    - Modal 컴포넌트 디자인(임시)
    
    * Feat: Modal 컴포넌트 동작 테스트를 위한 Test 컴포넌트 제작 (#39)
    
    - 버튼 클릭 시 모달 컴포넌트 렌더링
    
    * Feat: li -> button 태그 변경 (#39)
    
    - interactive하지 않은 요소에 onClick 이벤트를 넣을 시 onKeyDown 이벤트도 넣어야 하는 에러 발생
    - li 태그가 button 태그를 감싸는 형태로 변경
    
    * Feat: handleBackgroundClick -> handleClose 함수 이름 변경 (#39)
    
    - 백그라운드와 'x' 버튼 클릭 시 양쪽 다 handleBackgroundClick 함수가 동작되던 이슈가 존재
    - 둘을 포괄하는 이름을 사용하기 위해  handleBackgroundClick 함수의 이름을 handleClose로 변경
    
    * Rename: scss 파일 맨 앞글자 대문자로 변경하기 위해 삭제 후 push (#39)
    
    - modal.module.scss 삭제
    - test.module.scss 삭제
    
    * Rename: scss 파일 맨 앞글자를 대문자로 변경하기 위한 삭제 및 재생성 (#39)
    
    - modal.module.scss -> Modal.module.scss
    - test.module.scss -> Test.module.scss

---
+++ client/src/components/common/Modal.tsx
@@ -0,0 +1,63 @@
+import styles from './modal.module.scss'
+
+type Option = {
+  label: string
+  handleClick: () => void
+  id: number
+}
+
+type Props = {
+  modalData: {
+    title: string
+    options: Option[]
+  }
+  onClose: () => void
+}
+
+function Modal({ modalData, onClose }: Props): JSX.Element {
+  const handleClose = (
+    event: React.MouseEvent<HTMLDivElement> | React.MouseEvent<HTMLButtonElement>
+  ) => {
+    // event.target -> 실제로 클릭된 요소
+    // event.currentTarget -> 이벤트가 바인딩된 요소
+    // 백그라운드를 클릭했을 때, 그 클릭이 모달 자체를 클릭한 것인지 확인
+    if (event.target === event.currentTarget) {
+      onClose()
+    }
+  }
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    console.log(e)
+  }
+
+  return (
+    <>
+      <div
+        role='presentation'
+        className={styles.modalBackground}
+        onClick={handleClose}
+        onKeyDown={e => handleKeyDown(e)}
+      />
+      <div className={styles.modal}>
+        <div className={styles.modalTop}>
+          <div>{modalData.title}</div>
+          <button className={styles.xBtn} type='button' onClick={handleClose}>
+            𝖷
+          </button>
+        </div>
+        <ul className={styles.optionsContainer}>
+          {modalData.options.map(option => {
+            return (
+              <li key={option.id}>
+                <button type='button' onClick={option.handleClick}>
+                  {option.label}
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Modal
