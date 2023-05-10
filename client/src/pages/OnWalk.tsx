3675b12eb1d3e69605bb81a104c92bf5e73b5a85
iberis2 <snow9402@gmail.com>
2023-05-10 13:26:56 +0900

    Formatting: merge 충돌 방지를 위해 onWalk 컴포넌츠 초기화 (#27)
    UI 확인을 위해 onWalk 컴포넌츠 안에 Camera, SnapForm 컴포넌츠 넣어서 작업 했던 것 삭제

--- client/src/pages/OnWalk.tsx
+++ client/src/pages/OnWalk.tsx
@@ -1,11 +1,3 @@
-// import Camera from '../components/OnWalk/Camera'
-import SnapForm from '../components/OnWalk/SnapForm'
-
 export default function OnWalk() {
-  return (
-    <div>
-      {/* <Camera /> */}
-      <SnapForm />
-    </div>
-  )
+  return <div>OnWalk</div>
 }
