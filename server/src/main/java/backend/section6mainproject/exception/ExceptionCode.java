c61f3a905e7c23d87f8d224ce747d446de65e303
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-05-10 17:58:25 +0900

    Test: WalkLog delete 기능 구현 (#52)

--- server/src/main/java/backend/section6mainproject/exception/ExceptionCode.java
+++ server/src/main/java/backend/section6mainproject/exception/ExceptionCode.java
@@ -6,7 +6,7 @@ public enum ExceptionCode {
     WALK_LOG_NOT_RECORDING(400, "Not Recording Now"), //걷기 기록중이 아닙니다.
     MEMBER_NOT_FOUND(404, "Member Not Found"),// 존재하지 않는 회원
     WALK_LOG_NOT_FOUND(404,"WalkLog Not Found"),//존재하지 않는 걷기 기록
-    WALK_LOG_CONTENT_NOT_FOUND(404, "WalkLogContent Not Found"), //존재하지 않는 걷기 기록 컨텐츠;
+    WALK_LOG_CONTENT_NOT_FOUND(404, "WalkLogContent Not Found"), //존재하지 않는 걷기 기록 컨텐츠
     MEMBER_EXISTS(409, "Member exists");
 
     @Getter
