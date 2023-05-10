ce1af70623acd6f3970051402fdf281eacdfa93f
hajongon <hajongon@gmail.com>
2023-05-10 12:16:37 +0900

    Design: google map 커스터마이징 (#66)
    - 지도 노출 설정
    - 물(강, 바다, 호수 등) 색상 설정
    - 건물 이름 가리기
    - 지도 단순화
    - mapTypeControl OFF (지도/위성 선택 버튼 끄기)
    - streetViewControl OFF (로드뷰 버튼 끄기)

--- client/src/components/common/Map.tsx
+++ client/src/components/common/Map.tsx
@@ -67,7 +67,54 @@ function Map() {
     <>
       {isLoadMap ? (
         <LoadScript googleMapsApiKey={apiKey} onLoad={() => console.log('Loaded!')}>
-          <GoogleMap mapContainerStyle={containerStyle} zoom={14} center={currentLocation}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            zoom={14}
+            center={currentLocation}
+            options={{
+              styles: [
+                {
+                  // 색상
+                  featureType: 'all',
+                  stylers: [
+                    {
+                      saturation: -100,
+                    },
+                  ],
+                },
+                {
+                  // 물 색상
+                  featureType: 'water',
+                  stylers: [
+                    {
+                      color: '#7dcdcd',
+                    },
+                  ],
+                },
+                {
+                  // 건물 이름 가리기
+                  featureType: 'all',
+                  elementType: 'labels',
+                  stylers: [{ visibility: 'off' }],
+                },
+
+                {
+                  // 지도 단순화하기
+                  featureType: 'road',
+                  elementType: 'geometry',
+                  stylers: [
+                    { visibility: 'simplified' },
+                    { hue: '#000000' },
+                    { saturation: -50 },
+                    { lightness: -15 },
+                    { weight: 1.5 },
+                  ],
+                },
+              ],
+              mapTypeControl: false, // 지도 위성 끄기
+              streetViewControl: false, // 사람 모양 끄기
+            }}
+          >
             {currentLocation && <Marker position={currentLocation} />}
           </GoogleMap>
         </LoadScript>
