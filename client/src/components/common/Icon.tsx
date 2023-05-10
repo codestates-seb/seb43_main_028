8e5e2165148b88a4ab188d7d7bee4ffe744cce3f
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-10 13:24:46 +0900

    [FE] Feat: Sprite SVG로 Icon 컴포넌트 구현 (#63)
    * Design: Sprite SVG 파일 생성 (#62)
    
    * Feat: Icon 컴포넌트 구현 (#62)
    
    - Sprite SVG에 추가한 icon들을 타입 설정
    - 프롭스로 name, size를 받음
    
    * Design: 아이콘 추가 (#62)
    
    - arrow-left
    - no-image
    
    * Rename: SpriteSVG to SpriteIcon (#62)
    
    어떤 용도의 svg인지를 더 명확하게 작성

---
+++ client/src/components/common/Icon.tsx
@@ -0,0 +1,28 @@
+import SpriteIcon from '../../assets/sprite-icon.svg'
+
+type IconName =
+  | 'camera-color'
+  | 'camera-oval'
+  | 'close'
+  | 'edit-gray'
+  | 'map-color'
+  | 'no-profile'
+  | 'pin'
+  | 'refresh'
+  | 'time-gray'
+  | 'trash-gray'
+  | 'no-image'
+  | 'arrow-right'
+
+interface IconProps {
+  name: IconName
+  size?: 16 | 24 | 48
+}
+
+export default function Icon({ name, size = 24 }: IconProps) {
+  return (
+    <svg width={size} height={size}>
+      <use href={`${SpriteIcon}#${name}`} />
+    </svg>
+  )
+}
