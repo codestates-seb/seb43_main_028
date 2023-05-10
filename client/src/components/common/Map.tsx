4da40153823a0fc75a24dd45f0469d7cf28c5012
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-10 14:54:15 +0900

    [FE] Feat: 맵 컴포넌트 구현 (#70)
    * Chore: react-google-maps api 설치 (#66)
    
    - @react-google-maps/api 설치(dependencies)
    
    * Chore: google maps api key 환경변수 설정 (#66)
    
    - VITE_GOOGLE_MAPS_API_KEY 환경변수에 API 키 할당
    - .gitignore 파일에 .env 추가
    
    + 컴포넌트 이름 변경 map -> Map
    
    * Feat: 'maptest' Route 추가 (#66)
    
    - Map 컴포넌트 export
    - router의 index.tsx 수정
      - 'maptest' 경로 접근 시 Map 컴포넌트 렌더링
    
    * Feat: google map 렌더링 (#66)
    
    - @react-google-maps/api 를 이용한 google map 렌더링
    - VITE_GOOGLE_MAPS_API_KEY 환경변수 사용
    - 임시적으로 지도 center 설정(서울시청 좌표)
    
    * Feat: 현재 위치를 지도의 중앙에 보여주는 기능 (#66)
    
    - watchPosition 메서드 사용(기기의 현재 위치 탐색)
    - useEffect를 이용해 컴포넌트 렌더링 전 현재 위치 갱신
    
    * Feat: 걷는 경로에 따라 순차적으로 배열에 좌표 추가 (#66)
    
    - locations 배열을 state로 관리
    - prevLocation이 존재하지 않으면 location을 담은 배열로 locations 갱신
    - prevLocation이 존재하면 배열에 현재 위치를 추가
    
    * Test: 기기 이동에 따라 locations 배열에 좌표가 추가되는지 테스트 (#66)
    
    * Fix: 파일 이름 변경이 반영되지 않는 문제 해결을 위한 파일 삭제 (#66)
    
    - map.tsx -> Map.tsx 로 바꾸기 위해 파일 삭제 후 다시 생성해서 push할  예정
    
    * Fix: 파일명 변경을 위한 파일 삭제 및 재생성 (#66)
    
    - map.tsx에서 Map.tsx로 파일명을 변경하기 위해 map.tsx 파일을 삭제한 후 push하고, Map.tsx 파일을 생성한 후 push할 예정
    
    * Test: 기기 이동에 따라 좌표가 배열에 추가되는지의 여부 테스트 (#66)
    
    - 배포 후 모바일로 테스트를 해보기 위함
    - 지도 하단에 lat(위도), lng(경도) 표시
    
    * Design: Map.module.scss 파일 생성 (#66)
    
    - 테스트 레이아웃 설정
    
    * Test: 좌표 테스트를 위한 레이아웃 제작 (#66)
    
    - 현재 좌표
    - 좌표 리스트
    
    * Design: 테스트 레이아웃 디자인 (#66)
    
    - Map.module.scss
    
    * Design: google map 커스터마이징 (#66)
    
    - 지도 노출 설정
    - 물(강, 바다, 호수 등) 색상 설정
    - 건물 이름 가리기
    - 지도 단순화
    - mapTypeControl OFF (지도/위성 선택 버튼 끄기)
    - streetViewControl OFF (로드뷰 버튼 끄기)
    
    * Feat: 직전 좌표와의 거리를 계산할 수 있는 함수 추가 (#66)
    
    - getDistance 함수를 이용해 직전좌표와 현재 위치의 거리 계산
    
    * Feat: 기기 이동에 따른 경로 그리기 (#66)
    
    - react-google-maps/api의 Polyline 컴포넌트를 이용해 경로 그리기
    
    * Fix: 경로 그리기가 되지 않는 이슈 관련 테스트 (#66)
    
    - useEffect 의존성 배열에 locations 추가
    
    * Fix: 경로 그리기가 되지 않는 이슈 관련 테스트 - 2 (#67)
    
    - useEffect -> 버튼으로 동작하는 함수로 변경
    - dependency array에 의존하지 않게 하기 위함
    
    * Test: 배포 후 좌표 이동 테스트 (#66)
    
    - Vercel 배포 후 좌표 테스트
    
    * Test: 홈 화면에서 /maptest 경로로 link (#66)
    
    - Vercel에서 React Router가 제대로 작동하지 않는 이슈
    - 임시적으로 홈 화면에서 /maptest 경로로 이동할 수 있는 링크 생성
    
    * Feat: gestureHandling 옵션 추가 (#66)
    
    - 지도를 한 손가락으로 움직일 수 있는 옵션 추가

---
+++ client/src/components/common/Map.tsx
@@ -0,0 +1,210 @@
+import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api'
+import { useState, useEffect } from 'react'
+import styles from './Map.module.scss'
+
+// 좌표 테스트 ing
+
+const containerStyle = {
+  width: '340px',
+  height: '340px',
+}
+
+type LatLng = { lat: number; lng: number }
+
+// test
+function Map() {
+  const [apiKey, setApiKey] = useState('')
+  const [currentLocation, setCurrentLocation] = useState<LatLng | undefined>(undefined)
+  const [locations, setLocations] = useState<LatLng[]>([])
+  const [isLoadMap, setIsLoadMap] = useState(false)
+  const [distance, setDistance] = useState(0)
+
+  console.log(locations)
+  console.log(currentLocation)
+
+  useEffect(() => {
+    setIsLoadMap(true)
+    setApiKey(import.meta.env.VITE_GOOGLE_MAPS_API_KEY || '')
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        if (position) {
+          const location = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          }
+          setCurrentLocation(location)
+        }
+      },
+      function (error) {
+        alert(`Error occurred. Error code: ${error.code}`)
+      },
+      { timeout: 5000 }
+    )
+  }, [])
+
+  function getDistance(lat1: number, lng1: number, lat2: number, lng2: number): number {
+    function deg2rad(deg: number): number {
+      return deg * (Math.PI / 180)
+    }
+    const R = 6371 // Radius of the earth in km
+    const dLat = deg2rad(lat2 - lat1) // deg2rad below
+    const dLon = deg2rad(lng2 - lng1)
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    const d = (R * c) / 1000 // Distance in m
+    return d
+  }
+
+  function watchLocation() {
+    let prevLocation: LatLng | undefined // 이전 위치
+
+    if (navigator.geolocation) {
+      console.log('geolocation 실행')
+      // 기기의 현재 위치를 탐색하는 브라우저 api 사용
+      navigator.geolocation.watchPosition(
+        position => {
+          // 좌표를 담아둘 객체 정의
+          const location = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          }
+          // 현재 위치 갱신
+          setCurrentLocation(location)
+          // 직전 좌표가 없으면 현재 watchPosition으로 받은 location을 locations 배열에 추가
+          if (!prevLocation) {
+            setLocations([location])
+            prevLocation = location
+          }
+          // 직전 좌표가 있으면 locations 배열에 현재 위치 추가
+          if (currentLocation && prevLocation) {
+            // 직전 좌표가 존재하면 현재 좌표와 직전 좌표의 거리를 계산
+            const distanceFromLastPosition = getDistance(
+              currentLocation.lat,
+              currentLocation.lng,
+              prevLocation.lat,
+              prevLocation.lng
+            )
+
+            // distance 상태를 갱신
+            setDistance(distanceFromLastPosition)
+            if (distanceFromLastPosition >= 5) {
+              setLocations(prevLocations => [...prevLocations, location])
+              prevLocation = location
+            }
+          }
+        },
+        err => {
+          console.log(err)
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: 5000, // 5초마다 위치 정보 갱신 시도
+        }
+      )
+    } else {
+      console.log('Geolocation is not supported')
+    }
+    // vercel에서 설정한 환경 변수를 가져옴
+  }
+
+  return (
+    <>
+      {isLoadMap ? (
+        <LoadScript googleMapsApiKey={apiKey} onLoad={() => console.log('Loaded!')}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            zoom={14}
+            center={currentLocation}
+            options={{
+              styles: [
+                {
+                  // 색상
+                  featureType: 'all',
+                  stylers: [
+                    {
+                      saturation: -100,
+                    },
+                  ],
+                },
+                {
+                  // 물 색상
+                  featureType: 'water',
+                  stylers: [
+                    {
+                      color: '#7dcdcd',
+                    },
+                  ],
+                },
+                {
+                  // 건물 이름 가리기
+                  featureType: 'all',
+                  elementType: 'labels',
+                  stylers: [{ visibility: 'off' }],
+                },
+
+                {
+                  // 지도 단순화하기
+                  featureType: 'road',
+                  elementType: 'geometry',
+                  stylers: [
+                    { visibility: 'simplified' },
+                    { hue: '#000000' },
+                    { saturation: -50 },
+                    { lightness: -15 },
+                    { weight: 1.5 },
+                  ],
+                },
+              ],
+              mapTypeControl: false, // 지도 위성 끄기
+              streetViewControl: false, // 사람 모양 끄기
+              gestureHandling: 'greedy', // 한 손 가락으로 지도 핸들링
+            }}
+          >
+            {currentLocation && <Marker position={currentLocation} />}
+            {locations.length > 1 && (
+              <Polyline
+                path={locations}
+                options={{
+                  strokeColor: '#55de50',
+                  strokeOpacity: 1,
+                  strokeWeight: 6,
+                }}
+              />
+            )}
+          </GoogleMap>
+        </LoadScript>
+      ) : null}
+      <div className={styles.btnBox}>
+        <button type='button' className={styles.btn} onClick={watchLocation}>
+          watchPosition
+        </button>
+      </div>
+      <div className={styles.distanceBox}>
+        <div>현재 좌표: </div>
+        <div>
+          {currentLocation
+            ? `Lat: ${currentLocation.lat.toFixed(6)}, Lng: ${currentLocation.lng.toFixed(6)}`
+            : '-'}
+        </div>
+      </div>
+      <div className={styles.distanceBox}>
+        <div>직전 좌표와의 거리: </div>
+        <div>{distance.toFixed(2)}</div>
+        <div>m</div>
+      </div>
+      <div className={styles.locationInfoBox}>
+        <div>좌표 리스트</div>
+        {locations.map(location => {
+          return (
+            <p key={Math.random()}>{`${location.lat.toFixed(4)}, ${location.lng.toFixed(4)}`}</p>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default Map
