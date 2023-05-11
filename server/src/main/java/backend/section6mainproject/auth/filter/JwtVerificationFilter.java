f35abc184e67548388e63a3149fabc466a8f9745
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-05-11 13:42:03 +0900

    Feat: JWT를 이용한 로그인 기능 구현 (#59)

---
+++ server/src/main/java/backend/section6mainproject/auth/filter/JwtVerificationFilter.java
@@ -0,0 +1,97 @@
+package backend.section6mainproject.auth.filter;
+
+import backend.section6mainproject.auth.jwt.JwtTokenizer;
+import backend.section6mainproject.auth.utils.CustomAuthorityUtils;
+import backend.section6mainproject.exception.BusinessLogicException;
+import backend.section6mainproject.exception.ExceptionCode;
+import backend.section6mainproject.member.entity.Member;
+import backend.section6mainproject.member.repository.MemberRepository;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+@RequiredArgsConstructor
+public class JwtVerificationFilter extends OncePerRequestFilter {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+    private final MemberRepository memberRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            if(request.getServletPath().equals("/members/refresh")){
+                verifyRefreshToken(request, response);
+            } else {
+                verifyAccessToken(request);
+            }
+        } catch (SignatureException se) {
+            request.setAttribute("exception", se);
+        } catch (ExpiredJwtException ee) {
+            request.setAttribute("exception", ee);
+        } catch (Exception e) {
+            request.setAttribute("exception", e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+    private void verifyAccessToken(HttpServletRequest request) {
+        Map<String, Object> claims = verifyAccessJws(request);
+        setAuthenticationToContext(claims);
+    }
+    private void verifyRefreshToken(HttpServletRequest request, HttpServletResponse response) {
+        Long memberId = Long.parseLong(verifyRefreshJws(request));
+        String accessToken = delegateAccessToken(memberId);
+        response.setHeader("Authorization", "Bearer " + accessToken);
+    }
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String authorization = request.getHeader("Authorization");
+        String refresh = request.getHeader("Refresh");
+        return (authorization == null || !authorization.startsWith("Bearer")) &&
+                (!request.getServletPath().equals("/members/refresh") || refresh == null);
+    }
+    private Map<String, Object> verifyAccessJws(HttpServletRequest request) {
+        String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        return jwtTokenizer.getClaims(accessToken, base64EncodedSecretKey).getBody();
+    }
+    private String verifyRefreshJws(HttpServletRequest request) {
+        String refreshToken = request.getHeader("Refresh");
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        return (String) jwtTokenizer.getClaims(refreshToken, base64EncodedSecretKey).getBody().get("sub");
+    }
+    private void setAuthenticationToContext(Map<String, Object> claims) {
+        String memberId = (String) claims.get("sub");
+        List<String> roles = (List<String>) claims.get("roles");
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberId, null, authorityUtils.getAuthorities(roles));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
+    private String delegateAccessToken(Long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        Member member = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", member.getEmail());
+        claims.put("roles", member.getRoles());
+
+        setAuthenticationToContext(claims);
+
+        String subject = member.getMemberId().toString();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        return jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+    }
+
+}
---
+++ server/src/main/java/backend/section6mainproject/auth/handler/MemberAuthenticationSuccessHandler.java
@@ -0,0 +1,41 @@
+package backend.section6mainproject.auth.handler;
+
+import backend.section6mainproject.member.entity.Member;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class MemberAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private final AuthenticationSuccessHandlerUtils authenticationSuccessHandlerUtils;
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Member member = (Member) authentication.getPrincipal();
+
+        String accessToken = authenticationSuccessHandlerUtils.delegateAccessToken(member);
+        response.setHeader("Authorization", "Bearer " + accessToken);
+
+        if((Boolean) request.getAttribute("autoLogin")){
+            String refreshToken = authenticationSuccessHandlerUtils.delegateRefreshToken(member);
+            response.setHeader("Refresh", refreshToken);
+        }
+
+        setMemberIdToBody(response, member);
+    }
+
+    private void setMemberIdToBody(HttpServletResponse response, Member member) throws IOException {
+        Map<String, Long> loginResponse = new HashMap<>();
+        loginResponse.put("memberId", member.getMemberId());
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.getWriter().write(objectMapper.writeValueAsString(loginResponse));
+    }
+
+}
