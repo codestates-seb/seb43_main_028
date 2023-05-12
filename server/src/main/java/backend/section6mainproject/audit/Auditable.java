05c0f20693660ce5e492e545cb8511e359ed5564
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-05-12 18:22:20 +0900

    76 be 걷기 전체 조회 기능 구현 (#84)
    * feat: 걷기 전체 조회기능 구현 (#76)
    
    * refactor: 걷기 기록 등록 기능 responseBody 변경 (#76)
    
    * refactor: 걷기 등록시 멤버가 기록중이면 오류를 발생하도록 변경 (#76)
    
    * Feat: 걷기 기록 전체 조회기능 구현 (#76)
    
    * Feat: 걷기 기록 전체 조회기능 구현 (#76)
    
    * refactor: 맵 이미지 Column 추가 (#76)
    
    * Feat: 걷기 전체 조회 기능 구현 (#76)
    
    * Refactor: 불필요한 Optional 제거(#76)
    
    * Refactor: 질문 요구사항 반영(#76)
    
    * Refactor: 달력조회 리팩토링 및 getMyWalkLog 추가(#76)

--- server/src/main/java/backend/section6mainproject/audit/Auditable.java
+++ server/src/main/java/backend/section6mainproject/audit/Auditable.java
@@ -1,6 +1,7 @@
 package backend.section6mainproject.audit;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable {
     @CreatedDate
-    @Column(nullable = false, updatable = false)
+    @Column(nullable = false, updatable = false) //달력테스트가 끝난 후 nullable = false로 변경해주세요
     private LocalDateTime createdAt;
 
     @LastModifiedDate
--- server/src/main/java/backend/section6mainproject/exception/ExceptionCode.java
+++ server/src/main/java/backend/section6mainproject/exception/ExceptionCode.java
@@ -3,6 +3,7 @@ package backend.section6mainproject.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
+    WALK_LOG_ALREADY_RECORDING(400,"WalkLog already recording"),
     CAN_NOT_CHANGE_WALK_LOG(403,"WalkLog Can Not Change"),
     CAN_NOT_EXIT_WALK_LOG(403,"WalkLog Can Not Exit"),
     MEMBER_NOT_FOUND(404, "Member Not Found"),// 존재하지 않는 회원
