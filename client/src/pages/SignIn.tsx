2f8a078a5a336e38fa3bb86056daec327e68b005
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-14 12:35:48 +0900

    [FE] Feat: SingIn, SingUp 기능 구현 (#86)
    * Design: scss 파일 생성 및 함수 컴포넌트 이름 변경 (#80)
    
    - SignIn.module.scss 생성
    - SignUp.module.scss 생성
    - Signin -> SignIn 컴포넌트 이름 변경
    - SignUp -> SignUp 컴포넌트 이름 변경
    
    * Design: SignUp 컴포넌트 기본 레이아웃 (#80)
    
    - Display name, Email, Password 입력란
    - Sing up 버튼
    - Log in 페이지로 연결되는 버튼
    
    * Chore: axios 설치 (#80)
    
    * Feat: SignUp 컴포넌트 (#80)
    
    - signUp api 구현
    - SignUp 컴포넌트의 form에서 submit 시 signUp api 작동
    
    * Feat: SignIn 컴포넌트 및 signIn API 구현 (#80)
    
    - SignIn.tsx 기본 레이아웃 및 동작 구현
    - SignIn.module.scss 작성
    - user.ts 내 signIn API 작성
    
    * Refactor: SignUp.module.scss className 수정 (#80)
    
    - entireContainer -> container
    - container -> formBox
    
    * Feat: vite.config.ts 프록시 설정 (#80)
    
    - 개발 중 GET, POST 요청 테스트를 위해 vite 프록시 설정
    
    * Feat: signin, signup POST 요청 성공 (#80)
    
    - ngrok 서버에 요청을 보내고 응답으로 회원정보를 받아오는 것까지 테스트 완료
    
    * Feat: jotai atom로 유저 정보 변경 (#80)
    
    - 로그인 시 Home 컴포넌트로 이동하면서 콘솔에 userData가 출력되도록 함
    
    * Feat: 로그인 시 로컬스토리지에 refreshToken 저장 (#80)
    
    - signIn API 호출 시 saveRefreshTokenToLocalStorage 실행
    
    * Feat: refreshToken을 get, remove하는 함수 추가 (#80)
    
    * Refactor: useRouter를 이용한 navigate 처리 (#80)
    
    - 기존 signin과 signup에서 useNavigate를 사용해서 처리하던 기능을 useRouter 훅을 만들어 사용하는 것으로 변경
    
    * Feat: 로그인 상태 여부 확인하는 로직 구현 (#80)
    
    - path가 변경될 때마다 로그인 상태를 체크하는 로직 구현
    
    * Feat: 회원가입, 로그인 기능 (#80)
    
    - 회원가입
    - 로그인
    - access token, refresh token 관리
    - path 이동 시 로그인 여부 체크
    
    todo
    - email, 비밀번호 유효성 검사

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,3 +1,58 @@
-export default function Signin() {
-  return <div>Signin</div>
+import { Link, useNavigate } from 'react-router-dom'
+import { useAtom } from 'jotai'
+import { signIn, getCurrentUserInfo } from '../apis/user'
+import styles from './SignIn.module.scss'
+import { idAtom, isLoginAtom, userAtom } from '../store/authAtom'
+import useRouter from '../hooks/useRouter'
+
+function SignIn() {
+  const { routeTo } = useRouter()
+  const [, setUser] = useAtom(userAtom)
+  const [, setId] = useAtom(idAtom)
+  const [, setIsLogin] = useAtom(isLoginAtom)
+
+  const logInSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    const memberId = await signIn({ email, password, autoLogin: true })
+    setId(memberId)
+
+    if (memberId) {
+      const userInfoRes = await getCurrentUserInfo(`/api/members/${memberId}`)
+      // 전역 상태에 userInfoRes 저장
+      setIsLogin(true)
+      setUser(userInfoRes)
+      routeTo('/')
+      return
+    }
+    alert('Sorry, you failed to log in.')
+  }
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={logInSubmitHandler}>
+        <label className={styles.label}>
+          Email
+          <input className={styles.input} type='email' name='email' required />
+        </label>
+        <label className={styles.label}>
+          Password
+          <input className={styles.input} type='password' name='password' required />
+        </label>
+        <button className={styles.LoginBtn} type='submit'>
+          Log in
+        </button>
+        <span>
+          Don’t have an account?<Link to='/signup'>Sign up</Link>
+        </span>
+      </form>
+    </div>
+  )
 }
+
+export default SignIn
