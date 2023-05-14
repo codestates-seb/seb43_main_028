8b6f602d965e54abca3aba8f122904842257b66c
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-15 05:48:27 +0900

    Feat: routerData 생성 (#96)
    - lazy로 페이지 컴포넌트 import
    - router element 별로 탭바표시 여부, 인증요부 등의 정보를 포함하는 routerData 객체 생성
    - TapBar 정보를 관리하는 객체 생성

---
+++ client/src/router/routerData.tsx
@@ -0,0 +1,89 @@
+import { lazy } from 'react'
+import { TapBarElementITF } from '../types/TapBarElement'
+
+const Home = lazy(() => import('../pages/Home'))
+const Feed = lazy(() => import('../pages/Feed'))
+const MyPage = lazy(() => import('../pages/MyPage'))
+const HistoryList = lazy(() => import('../pages/HistoryList'))
+const HistoryDetail = lazy(() => import('../pages/HistoryDetail'))
+const OnWalk = lazy(() => import('../pages/OnWalk'))
+const AfterWalk = lazy(() => import('../pages/AfterWalk'))
+const SignIn = lazy(() => import('../pages/SignIn'))
+const SignUp = lazy(() => import('../pages/SignUp'))
+const NotFound = lazy(() => import('../pages/NotFound'))
+
+interface RouterElement {
+  id: number
+  path: string
+  label: string
+  element: React.ReactNode
+  onTapBar: boolean
+  withAuth: boolean
+}
+
+export const routerData: RouterElement[] = [
+  { id: 0, path: '/', label: '홈', element: <Home />, onTapBar: true, withAuth: false },
+  {
+    id: 3,
+    path: '/history',
+    label: '기록',
+    element: <HistoryList />,
+    onTapBar: true,
+    withAuth: true,
+  },
+  {
+    id: 4,
+    path: '/history/:id',
+    label: '기록상세',
+    element: <HistoryDetail />,
+    onTapBar: false,
+    withAuth: true,
+  },
+  { id: 1, path: '/feed', label: '피드', element: <Feed />, onTapBar: true, withAuth: false },
+  { id: 2, path: '/mypage', label: '내정보', element: <MyPage />, onTapBar: true, withAuth: true },
+  {
+    id: 5,
+    path: '/onwalk',
+    label: '걷기중',
+    element: <OnWalk />,
+    onTapBar: false,
+    withAuth: false,
+  },
+  {
+    id: 6,
+    path: '/afterwalk',
+    label: '걷기완료',
+    element: <AfterWalk />,
+    onTapBar: false,
+    withAuth: false,
+  },
+  {
+    id: 7,
+    path: '/signin',
+    label: '로그인',
+    element: <SignIn />,
+    onTapBar: false,
+    withAuth: false,
+  },
+  {
+    id: 8,
+    path: '/signup',
+    label: '회원가입',
+    element: <SignUp />,
+    onTapBar: false,
+    withAuth: false,
+  },
+  { id: 9, path: '*', label: '404', element: <NotFound />, onTapBar: false, withAuth: false },
+]
+
+export const TapBarContent: TapBarElementITF[] = routerData.reduce((prev, router) => {
+  if (!router.onTapBar) return prev
+  return [
+    ...prev,
+    {
+      id: router.id,
+      path: router.path,
+      label: router.label,
+    },
+  ]
+}, [] as TapBarElementITF[])
