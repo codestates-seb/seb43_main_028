ef4f317a6ef2c67954f023081acd193a0a3b59a7
snow <120469477+iberis2@users.noreply.github.com>
2023-05-14 12:32:48 +0900

    [FE] Feat: 기록 리스트 UI  (#82)
    * Design: 화살표, ⋮점 아이콘 추가 (#73)
    
    * Design: 전역에 설정된 body width 값 삭제 (#73)
    
    * Feat: 기록 리스트 최신순으로 보기 UI 1차 완성 (#73)
    
    두 줄 이상 넘어가는 글자 ... 으로 자르기 css 추가 구현 필요
    
    * Feat: 더미데이터의 createdAt을 Date 형식의 string으로 바꿈 (#73)
    
    배포시 UTC 와의 시차 계산을 위해, Date 로 바꾸기 쉽도록
    '2023년 5월 10일' 을 '2023-05-10'으로 바꿈
    
    * Feat: 기록 더미 데이터를 useState 안에 담음 (#73)
    
    달력 구현하면 data 가 날짜 클릭할 때마다 달라지는 상태값이 되므로,
    더미 데이터를 useState 에 담고, state를 props로 내려줌
    
    * Feat: 달력 UI 구현 (#73)
    
    월별 보기 클릭시 제일 최신 기록의 날짜를 props로 받아서 달력이 나타나도록 UI 구현
    기록이 하나도 없으면 오늘 달력이 나타나도록 props의 기본값은 Date.now()로 구현
    
    * Design: 기록 리스트 최신순으로 보기 UI 완성 (#73)
    
    CSS 2 줄 이상 말줄임
    snap time 옆 시계 아이콘 추가
    
    * Formatting: 불필요한 주석들 삭제 (#73)
    
    * Refactor: 코드 가독성을 위해 Calendar 컴포넌츠 분리 (#73)
    
    Calendar 컴포넌츠 코드를 연월 / 요일 / 날짜  로 각 컴포넌츠 분리
    
    * Feat: 달력에 today 표시와 걷기 기록 있는 날 표시 구현 (#73)
    
    가장 최근 데이터의 날짜만 props로 내리던 코드에서 전체 데이터를 props로 내리고 그 중 날짜들만 배열로 추출해서
    달력의 해당 일의 date 객체와 비교해서 일치하는 날짜에 표시하는 걸로 변경
    
    * Feat: 달력 버튼 누르면 이전 달, 다음 달 조회 기능 추가 (#73)
    
    연, 월을 state 로 바꿔서 버튼 클릭시미다 변경되는 값으로 바꿈
    
    * Design: today 표시 영문에서 밑줄로 변경 (#73)
    
    * Feat: 시간 표시를 string 대신 date 형식에서 계산하도록 변경 (#73)
    
    '1시간 10분' 형식의 string이 아닌 시작시간, 종료시간의 Date 객체 형식
    '2023-05-01T17:00:19'
    을 받아서 시차를 계산 하도록 변경
    
    * Formatting: 더미 데이터 삭제 (#73)
    
    * Design: sprite-icon.svg 수정 (#73)
    
    arrow-left, three-dot svg viewBox 24인 svg로 수정
    
    * Design: 달력 CSS 반응형 UI 수정 (#73)
    
    - 달력에는 좌우 padding이 필요 없으므로
    전체에 padding 20px → 토글, 기록리스트만 좌우 margin 20px로 수정
    - 캘린더 날짜 좌우 정렬, today, 기록 표시 세로 중앙 정렬되도록 display flex 추가
    - 반응형 디스플레이에서 깨지지 않도록 today 표시, 기록 표시 % → px로 변경

---
+++ client/src/components/HistoryList/History.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import Icon from '../common/Icon'
+import styles from './History.module.scss'
+import HistoryItem from './HistoryItem'
+import { getHourMinuteDiff } from '../../utils/ date'
+
+interface ItemITF {
+  id: number
+  createdAt: string
+  imageUrl: string
+  text: string
+}
+
+type HistoryItemProps = {
+  data: {
+    id: number
+    mapImg: string
+    startAt: string
+    endAt: string
+    message: string
+    walkLogContents: ItemITF[]
+  }
+}
+
+export default function History({ data }: HistoryItemProps) {
+  const [moreContent, setMore] = useState(false)
+  const { mapImg, startAt, endAt, message, walkLogContents } = data
+  const date = new Date(startAt)
+  const Year = date.getFullYear()
+  const Month = date.getMonth() + 1
+  const Day = date.getDate()
+  const timeDiff = new Date(endAt).getTime() - new Date(startAt).getTime()
+  const time = getHourMinuteDiff(timeDiff)
+  const moreContents = walkLogContents.slice(1)
+
+  const handleMore = () => {
+    setMore(true)
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.mapTimeBox}>
+        <img src={mapImg} className={styles.map} alt='지도 이미지' />
+        <div>
+          <p className={styles.date}>
+            {Year}년 {Month}월 {Day}일
+          </p>
+          <p className={styles.time}>{time}</p>
+        </div>
+      </div>
+      <p className={styles.message}>{message}</p>
+      <HistoryItem item={walkLogContents[0]} startAt={startAt} />
+      {!moreContent && (
+        <button type='button' className={styles.moreBtn} onClick={handleMore}>
+          <Icon name='three-dot' size={24} /> {walkLogContents.length - 1} more
+        </button>
+      )}
+      {moreContent &&
+        moreContents.map(item => {
+          return <HistoryItem key={item.id} item={item} startAt={startAt} />
+        })}
+      <button type='button' className={styles.detailBtn}>
+        자세히 보기
+      </button>
+    </div>
+  )
+}
