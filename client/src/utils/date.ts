6a431571d460d78c5fcf1e29c44d3d9546058df2
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-15 02:33:35 +0900

    Feat: 걸은 시간을 표현하기위한 유틸함수 추가 (#94)
    - 1시간 이상: *시간 *분(0분일 경우 시간만 표시)
    - 1시간 미만, 1분 이상: *분
    - 1분 미만: *초
    
    - 순간기록의 총 걸은 시간이 1분 미만일 경우 제대로 안나오는 부분 수정을 위한 함수 (utils/date.js => passedHourMinuteSecondFormat 함수)

--- client/src/utils/date.ts
+++ client/src/utils/date.ts
@@ -1,17 +1,57 @@
-export default function getHourMinuteDiff(timeDiff: number) {
-  const aMinute = 1000 * 60
-  const anHour = 1000 * 60 * 60
+const A_MINUTE = 1000 * 60
+const AN_HOUR = 1000 * 60 * 60
+const A_SECOND = 1000
 
-  if (timeDiff >= anHour) {
-    const hour = Math.floor(timeDiff / anHour)
-    const rest = timeDiff % anHour
-    const minute = Math.floor(rest / aMinute) ? `${Math.floor(rest / aMinute)} 분` : ''
-    return `${hour} 시간 ${minute} `
-  }
-  if (timeDiff >= aMinute) {
-    const minute = Math.floor(timeDiff / aMinute)
-    return `${minute} 분`
-  }
+export function passedHourMinuteSecondFormat(timeDiff: number) {
+  const hour = Math.floor(timeDiff / AN_HOUR)
+  const restOfHour = timeDiff % AN_HOUR
+  const minute = Math.floor(restOfHour / A_MINUTE)
+  const restOfminute = restOfHour % A_MINUTE
+  const second = Math.floor(restOfminute / A_SECOND)
 
-  return '0분'
+  const formattedHour = hour ? `${hour}시간` : ''
+  const formattedMinute = minute ? `${minute}분` : ''
+  const formattedSecond = second ? `${second}초` : ''
+
+  return (
+    !formattedHour && !formattedMinute ? formattedSecond : `${formattedHour} ${formattedMinute}`
+  ).trim()
+}
+
+export function dateFormat(date: Date) {
+  const month: number = date.getMonth() + 1
+  const day: number = date.getDate()
+
+  const formattedMonth: string = month >= 10 ? `${month}` : `0${month}`
+  const formattedDay: string = day >= 10 ? `${day}` : `0${day}`
+
+  return `${date.getFullYear()}-${formattedMonth}-${formattedDay}`
+}
+
+export function timeFormat(date: Date) {
+  const hour: number = date.getHours()
+  const minute: number = date.getMinutes()
+  const second: number = date.getSeconds()
+
+  const formattedHour: string = hour >= 10 ? `${hour}` : `0${hour}`
+  const formattedMinute: string = minute >= 10 ? `${minute}` : `0${minute}`
+  const formattedSecond: string = second >= 10 ? `${second}` : `0${second}`
+
+  return formattedHour === '00'
+    ? `${formattedMinute}:${formattedSecond}`
+    : `${formattedHour}:${formattedMinute}:${formattedSecond}`
+}
+
+export function timerFormat(seconds: number) {
+  const hour: number = Math.floor(seconds / 3600)
+  const minute: number = Math.floor((seconds % 3600) / 60)
+  const second: number = Math.floor((seconds % 3600) % 60)
+
+  const formattedHour: string = hour >= 10 ? `${hour}` : `0${hour}`
+  const formattedMinute: string = minute >= 10 ? `${minute}` : `0${minute}`
+  const formattedSecond: string = second >= 10 ? `${second}` : `0${second}`
+
+  return formattedHour === '00'
+    ? `${formattedMinute}:${formattedSecond}`
+    : `${formattedHour}:${formattedMinute}:${formattedSecond}`
 }
