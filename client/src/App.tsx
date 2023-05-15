6db96dacfc46c8768d02b9488fb40cd7876c3d5e
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-15 14:36:29 +0900

    Feat: 리액트 쿼리 초기 설정 (#96)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import './styles/global.scss'
 
 function App() {
-  return <RouterProvider router={router} />
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
