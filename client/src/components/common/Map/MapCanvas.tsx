7e0eee145d05052954526ae47b169f00faa9e217
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-18 07:57:44 +0900

    Feat : useMarker() 구현 (#98)
    - MapCanvas에 Marker를 그려주는 커스텀 훅 생성

--- client/src/components/common/Map/MapCanvas.tsx
+++ client/src/components/common/Map/MapCanvas.tsx
@@ -1,11 +1,16 @@
 import { forwardRef } from 'react'
+import { useGoogleMap } from '@ubilabs/google-maps-react-hooks'
+import useMarker from '../../../hooks/useMarker'
 
 type MapCanvasProps = {
   style: React.CSSProperties
+  position: google.maps.LatLngLiteral | null
 }
 
 const MapCanvas = forwardRef<HTMLDivElement, MapCanvasProps>((props, ref) => {
-  const { style } = props
+  const { style, position, path } = props
+  const map = useGoogleMap() || null
+  useMarker({ map, position })
 
   return <div ref={ref} style={style} />
 })
---
+++ client/src/hooks/useMarker.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from 'react'
+
+type UseMarkerType = {
+  map: google.maps.Map | null
+  position: google.maps.LatLngLiteral | null
+}
+
+export default function useMarker({ map, position }: UseMarkerType) {
+  const markerRef = useRef<google.maps.Marker | null>(null)
+
+  useEffect(() => {
+    if (!map || markerRef.current) return
+
+    markerRef.current = new google.maps.Marker({
+      map,
+      position,
+      visible: true,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 10,
+        fillColor: '#8cff9e',
+        fillOpacity: 1,
+        strokeWeight: 4,
+        strokeColor: 'white',
+      },
+    })
+
+    position && map?.panTo(position)
+  }, [map, position])
+}
