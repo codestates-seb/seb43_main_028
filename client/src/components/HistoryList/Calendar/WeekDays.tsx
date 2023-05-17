1113ebfafdd1f57434ac5a021e72700f90b5ab91
snow <120469477+iberis2@users.noreply.github.com>
2023-05-17 14:55:49 +0900

    [FE] Refactor: 기록리스트 & 달력 컴포넌츠 리팩토링 (#116)
    * Refactor: key를 index → crypto.randomUUID변경 (#102)
    
    * Formatting: 타입스크립트 컨벤션 적용 (#102)
    
    Interface → Type
    
    * Refactor: 가독성을 위해 최신순/월별 토글 분리 (#102)
    
    * Refactor: 요일 className css로 토요일, 일요일 처리 (#102)
    
    * Refactor: 캘린더 Date 객체 → date-fns 라이브러리 사용 (#102)
    
    - tree shaking을 위해 date-fns 파일에 각 함수별로  import 모아놓음
    - 이전달, 다음달 바꾸는 onClick 이벤트 함수 handleMonth로 하나로 합침
    
    * Feat: 달력에서 날짜 선택 이벤트 추가 (#102)
    
    - 날짜 선택시 해당 날짜의 기록만 가져올 수 있도록 날짜 선택 이벤트 핸들러 추가
    - 코드 가독성을 위해 Dates 컴포넌츠 분리
    
    * Design: 달력과 기록 리스트 사이 margin 추가 (#102)
    
    * Design: 달력 날짜들 일직선에 위치하도록 정렬 (#102)
    
    history, today가 아래에 표시되는지에 따라 날짜가 위로 올라가서
    history, today position을 absolute 로 띄워서 별도로 위치시킴
    
    * Formatting: 코드 가독성을 위해 함수 이름 줄임 (#102)
    
    handleSelectDate() 함수 이름을
    Dates 컴포넌츠 안에서는 Date 선택하는 건지 알기 쉬우므로 handleSelect로 변경

--- client/src/components/HistoryList/Calendar/WeekDays.tsx
+++ client/src/components/HistoryList/Calendar/WeekDays.tsx
@@ -6,16 +6,8 @@ export default function WeekDays() {
     <thead>
       <tr>
         {weekdays.map(weekday => {
-          let day
-          if (weekday === '일') {
-            day = 'sunday'
-          } else if (weekday === '토') {
-            day = 'saturday'
-          } else {
-            day = 'day'
-          }
           return (
-            <td key={weekday} className={styles[day]}>
+            <td key={weekday} className={styles[weekday]}>
               {weekday}
             </td>
           )
