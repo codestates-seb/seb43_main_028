1ca0da9c5fe7f0963537d8b2b1fb837eebbb7f60
snow <120469477+iberis2@users.noreply.github.com>
2023-05-17 14:55:49 +0900

    [FE] Refactor: 기록리스트 & 달력 컴포넌츠 리팩토링 (#116)
    * Refactor: key를 index → crypto.randomUUID변경 (#102)
    
    * Formatting: 타입스크립트 컨벤션 적용 (#102)
    
    Interface → Type
    
    * Refactor: 가독성을 위해 최신순/월별 토글 분리 (#102)
    
    * Refactor: 요일 className css로 토요일, 일요일 처리 (#102)
    
    * Refactor: 캘린더 Date 객체 → date-fns 라이브러리 사용 (#102)
    
    - tree shaking을 위해 date-fns 파일에 각 함수별로  import 모아놓음
    - 이전달, 다음달 바꾸는 onClick 이벤트 함수 handleMonth로 하나로 합침
    
    * Feat: 달력에서 날짜 선택 이벤트 추가 (#102)
    
    - 날짜 선택시 해당 날짜의 기록만 가져올 수 있도록 날짜 선택 이벤트 핸들러 추가
    - 코드 가독성을 위해 Dates 컴포넌츠 분리
    
    * Design: 달력과 기록 리스트 사이 margin 추가 (#102)
    
    * Design: 달력 날짜들 일직선에 위치하도록 정렬 (#102)
    
    history, today가 아래에 표시되는지에 따라 날짜가 위로 올라가서
    history, today position을 absolute 로 띄워서 별도로 위치시킴
    
    * Formatting: 코드 가독성을 위해 함수 이름 줄임 (#102)
    
    handleSelectDate() 함수 이름을
    Dates 컴포넌츠 안에서는 Date 선택하는 건지 알기 쉬우므로 handleSelect로 변경

---
+++ client/src/components/HistoryList/Calendar/Dates.tsx
@@ -0,0 +1,75 @@
+import styles from './Dates.module.scss'
+import { getWeekRows, startOfToday, isEqual, format } from './date-fns'
+
+type SelectedDateProps = {
+  day: Date
+  histories: number[]
+  handleSelect: (day: Date) => void
+}
+
+function SelectedDate({ day, histories, handleSelect }: SelectedDateProps) {
+  const isHistory = histories.map(history => {
+    if (isEqual(history, day)) {
+      return <div key={history} className={styles.dot} />
+    }
+    return ''
+  })
+
+  return (
+    <button type='button' onClick={() => handleSelect(day)} className={styles.selected}>
+      {format(day, 'dd')}
+      {isEqual(startOfToday(), day) && <div className={styles.today} />}
+      {isHistory}
+    </button>
+  )
+}
+
+function UnSelectedDate({ day, histories, handleSelect }: SelectedDateProps) {
+  const isHistory = histories.map(history => {
+    if (isEqual(history, day)) {
+      return <div key={history} className={styles.dot} />
+    }
+    return ''
+  })
+
+  return (
+    <button type='button' onClick={() => handleSelect(day)} className={styles.unSelected}>
+      {format(day, 'dd')}
+      {isEqual(startOfToday(), day) && <div className={styles.today} />}
+      {isHistory}
+    </button>
+  )
+}
+
+type DatesProps = {
+  date: Date
+  selectDate: Date | null
+  histories: number[]
+  handleSelect: (day: Date) => void
+}
+
+export default function Dates({ date, selectDate, handleSelect, histories }: DatesProps) {
+  return (
+    <tbody>
+      {getWeekRows(date).map(week => {
+        return (
+          <tr key={crypto.randomUUID()}>
+            {week.map(day => {
+              if (day === 0) return <td key={crypto.randomUUID()} />
+
+              return (
+                <td key={crypto.randomUUID()} className={styles.container}>
+                  {selectDate && isEqual(day, selectDate) ? (
+                    <SelectedDate day={day} histories={histories} handleSelect={handleSelect} />
+                  ) : (
+                    <UnSelectedDate day={day} histories={histories} handleSelect={handleSelect} />
+                  )}
+                </td>
+              )
+            })}
+          </tr>
+        )
+      })}
+    </tbody>
+  )
+}
