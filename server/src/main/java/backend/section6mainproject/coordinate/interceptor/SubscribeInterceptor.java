ee34e865d6995164ea0eb3e405c836a5093f0b6b
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-05-17 12:00:43 +0900

    Refactor: 비즈니스 로직 예외 발생시 로깅 추가 (#125) (#126)
    * Refactor: 비즈니스 로직 예외 발생시 로깅 추가 (#125)
    
    * Fix: CORS 설정 원상 복구 (#125)

--- server/src/main/java/backend/section6mainproject/coordinate/interceptor/SubscribeInterceptor.java
+++ server/src/main/java/backend/section6mainproject/coordinate/interceptor/SubscribeInterceptor.java
@@ -8,6 +8,9 @@ import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.simp.SimpAttributesContextHolder;
 import org.springframework.messaging.support.ChannelInterceptor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubscribeInterceptor implements ChannelInterceptor {
     /**
      * 현재 기록중인 walkLog의 좌표만 확인할 수 있도록 subscribe url을 검증하는 메서드
@@ -17,8 +20,12 @@ public class SubscribeInterceptor implements ChannelInterceptor {
         MessageHeaders headers = message.getHeaders();
         if (headers.get("stompCommand").toString().equals("SUBSCRIBE")) {
             Object walkLogId = SimpAttributesContextHolder.getAttributes().getAttribute("walkLogId");
-            if (!headers.get("simpDestination").equals("/sub/" + walkLogId)) {
-                throw new BusinessLogicException(ExceptionCode.WALK_LOG_NOT_FOUND); // 인증 구현 후 ExceptionCode 추가 예정
+            Object destination = headers.get("simpDestination");
+            if (!destination.equals("/sub/" + walkLogId)) {
+                Map<String, Object> attr = new HashMap<>();
+                attr.put("dest", destination);
+                attr.put("requiredId", walkLogId);
+                throw new BusinessLogicException(ExceptionCode.WALK_LOG_NOT_FOUND, attr);
             }
         }
 
--- server/src/main/java/backend/section6mainproject/exception/BusinessLogicException.java
+++ server/src/main/java/backend/section6mainproject/exception/BusinessLogicException.java
@@ -1,13 +1,26 @@
 package backend.section6mainproject.exception;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
-public class BusinessLogicException extends RuntimeException{
-    @Getter
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Getter
+public class BusinessLogicException extends RuntimeException {
     private ExceptionCode exceptionCode;
+    private Map<String, Object> attributes = new HashMap<>();
 
     public BusinessLogicException(ExceptionCode exceptionCode) {
+        this(exceptionCode, null);
+    }
+
+    public BusinessLogicException(ExceptionCode exceptionCode, Map<String, Object> attributes) {
         super(exceptionCode.getMessage());
         this.exceptionCode = exceptionCode;
+        Optional.ofNullable(attributes).ifPresent(attr -> this.attributes.putAll(attr));
+        log.info("exception code : {}, attributes : {}", this.exceptionCode, this.attributes);
     }
 }
