601958e3c3b9e9857458684b0d707fb9ef6d8b7b
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-18 07:58:13 +0900

    Feat : usePolyline() 구현 (#98)
    - MapCanvas에 경로를 그려주는 커스텀 훅 생성

--- client/src/components/common/Map/MapCanvas.tsx
+++ client/src/components/common/Map/MapCanvas.tsx
@@ -1,16 +1,19 @@
 import { forwardRef } from 'react'
 import { useGoogleMap } from '@ubilabs/google-maps-react-hooks'
 import useMarker from '../../../hooks/useMarker'
+import usePolyline from '../../../hooks/usePolyline'
 
 type MapCanvasProps = {
   style: React.CSSProperties
   position: google.maps.LatLngLiteral | null
+  path: google.maps.LatLngLiteral[] | null
 }
 
 const MapCanvas = forwardRef<HTMLDivElement, MapCanvasProps>((props, ref) => {
   const { style, position, path } = props
   const map = useGoogleMap() || null
   useMarker({ map, position })
+  usePolyline({ map, path })
 
   return <div ref={ref} style={style} />
 })
---
+++ client/src/hooks/usePolyline.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react'
+
+type UsePolylineType = {
+  map: google.maps.Map | null
+  path: google.maps.LatLngLiteral[] | null
+}
+
+export default function usePolyline({ map, path }: UsePolylineType) {
+  const polylineRef = useRef<google.maps.Polyline | null>(null)
+
+  useEffect(() => {
+    if (map) {
+      polylineRef.current = new google.maps.Polyline({
+        map,
+        path,
+        strokeColor: '#8cff9e',
+        strokeOpacity: 1.0,
+        strokeWeight: 6,
+      })
+    }
+  }, [map, path])
+}
