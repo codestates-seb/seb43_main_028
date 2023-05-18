cf4ec2339dd05d2c58a0046d6b2bd369415bef60
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-18 16:56:16 +0900

    [FE] Feat: 마이페이지 기능 구현 및 디자인 (#146)
    * Design: 마이페이지 디자인 (#113)
    
    - 마이페이지 레이아웃(하드코딩)
    - DropDown 컴포넌트 생성
    
    * Rename: arrow-drop-down-16 파일 삭제 (#113)
    
    * Design: 마이페이지 프로필 수정 팝업창 디자인 (#113)
    
    * Feat: 유저 기본 데이터 마이페이지에 연결 (#113)
    
    - fix: userAtom 타입 수정
    
    * Feat: 회원 정보 수정 기능 구현 (#113)
    
    - formData를 이용해 PATCH 요청
    
    * Feat: 비밀번호 변경 기능 구현 (#113)
    
    todo
    - 비밀번호 변경 API 연결
    
    * Feat: 비밀번호 변경 API 요청 기능 구현 (#113)
    
    todo
    - PasswordChanged 컴포넌트 렌더링되지 않는 오류 해결
    
    * Fix: PasswordChanged 컴포넌트가 렌더링 되지 않는 이슈 해결 (#113)
    
    - 비밀번호 변경 시 event.preventDefault 추가
    
    * Design: 프로필 사진 프리뷰 Clear 버튼 디자인 추가 (#113)
    
    * Design: 프로필 사진 프리뷰 Clear 버튼 z-index 제거 (#113)
    
    * Chore: browser-image-compression 설치 (#113)
    
    * Test: 카메라 버튼 클릭 시 사진 업로드 창이 열리는지 테스트 (#113)
    
    * Feat: 이미지 압축 기능 구현 (#113)
    
    - 4mb 이상의 사진에 대해 압축
    
    * Fix: defaultWalkLogSetting 오류 해결 (#113)
    
    - 데이터 형식 맞지 않는 문제 (formData로 변경)
    
    * Design: 드롭다운에 의해 html 요소가 아래로 밀리는 이슈 해결 (#113)
    
    * Fix: form nesting 이슈 해결 (#113)
    
    - signup 컴포넌트 내 form이 이중으로 작성되어 있는 문제 해결
    
    * Design: icon 추가 및 이미지 크기 변경 (#113)
    
    - no-profile 추가
    - arrow-drop-down 크기 변경

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,19 +1,98 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import Icon from '../components/common/Icon'
 import styles from './SignUp.module.scss'
 import { signUp } from '../apis/user'
 import useRouter from '../hooks/useRouter'
+import TermsOfUse from '../components/SignUp/TermsOfUse'
 
-function Form() {
-  const [nickname, setNickname] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+type FormValueType = {
+  nickname: string
+  email: string
+  password: string
+}
+
+function SignUp() {
+  // 이용약관 관련 state
+  const [allCheck, setAllCheck] = useState(false)
+  const [useCheck, setUseCheck] = useState(false)
+  const [privacyCheck, setPrivacyCheck] = useState(false)
+
+  const emailReg =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
+  const passwordReg = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{10,}$/
+  const nicknameReg = /^[가-힣]{2,}$|^[a-zA-Z]{4,}$|^[가-힣a-zA-Z]{4,}$/
 
   const { routeTo } = useRouter()
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    signUp({ nickname, email, password }).then(res => {
+  // register: input 요소를 React Hook Form과 연결해 검증 규칙을 적용할 수 있게 하는 메서드
+  // formState: form state에 관한 정보를 담고 있는 객체
+  // errors: input 값들의 에러 정보를 가지고 있는 객체
+  // getValues: input 값을 가져올 수 있는 함수
+
+  const {
+    register,
+    handleSubmit,
+    formState: { dirtyFields, errors },
+    getValues,
+    // mode
+    // onChange: input 값이 바뀔 때마다 검증 로직 동작
+    // onBlur: 포커스 상태를 잃을 때 동작
+    // onSubmit: submit 함수가 실행될 때 동작
+    // onTouched: 첫 번째 blur 이벤트에서 동작하고 그 후에는 모든 change 이벤트에서 동작
+    // all: blur 및 change 이벤트에서 동작
+  } = useForm<FormValueType>({ mode: 'onChange' })
+
+  const {
+    onChange: emailOnChange,
+    onBlur: emailOnBlur,
+    name: emailName,
+    ref: emailRef,
+  } = register('email', {
+    // required: '이메일을 입력하세요',
+    pattern: {
+      value: emailReg,
+      message: '이메일 형식이 올바르지 않습니다.',
+    },
+  })
+
+  const {
+    onChange: nicknameOnChange,
+    onBlur: nicknameOnBlur,
+    name: nicknameName,
+    ref: nicknameRef,
+  } = register('nickname', {
+    // required: '닉네임을 입력하세요',
+    pattern: {
+      value: nicknameReg,
+      message: '닉네임은 한글 2글자 이상 또는 영어 4글자 이상이어야 합니다.',
+    },
+  })
+
+  const {
+    onChange: passwordOnChange,
+    onBlur: passwordOnBlur,
+    name: passwordName,
+    ref: passwordRef,
+  } = register('password', {
+    // required: '비밀번호를 입력하세요',
+    minLength: {
+      value: 10,
+      message: '비밀번호는 총 10자 이상이어야 합니다.',
+    },
+    maxLength: {
+      value: 15,
+      message: '비밀번호는 15자 이하여야 합니다.',
+    },
+    pattern: {
+      value: passwordReg,
+      message: '영소문자, 숫자, 특수문자가 각각 한 개 이상 포함되어야 합니다.',
+    },
+  })
+
+  const submitSignUpData = (data: any) => {
+    console.log(data)
+    signUp(data).then(res => {
       if (res === 'success') {
         routeTo('/signin')
       } else if (res === '409-fail') {
@@ -25,63 +104,93 @@ function Form() {
   }
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <label className={styles.label}>
-        Display name
+    <form
+      className={styles.formContainer}
+      onSubmit={handleSubmit(data => {
+        submitSignUpData(data)
+      })}
+    >
+      <div className={styles.inputBox}>
+        <label className={styles.label} htmlFor='email'>
+          이메일
+        </label>
         <input
+          id='email'
           className={styles.input}
-          type='text'
-          name='displayName'
-          value={nickname}
-          onChange={e => setNickname(e.target.value)}
-          required
+          placeholder='이메일을 입력해주세요.'
+          onChange={emailOnChange}
+          onBlur={emailOnBlur}
+          name={emailName}
+          ref={emailRef}
         />
-      </label>
-      <label className={styles.label}>
-        Email
+        <div className={styles.errorWrapper}>
+          {dirtyFields.email && errors.email ? (
+            <span className={styles.error}>{errors.email.message}</span>
+          ) : (
+            <div className={styles.noError} />
+          )}
+        </div>
+      </div>
+      <div className={styles.inputBox}>
+        <label className={styles.label} htmlFor='password'>
+          비밀번호
+        </label>
         <input
-          className={styles.input}
-          type='email'
-          name='email'
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label className={styles.label}>
-        Password
-        <input
+          id='password'
+          placeholder='10~15자리 영대•소문자, 숫자, 특수문자 조합'
           className={styles.input}
           type='password'
-          name='password'
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          pattern='^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$'
-          title='최소 8자, 문자와 숫자를 모두 포함해야 합니다.'
-          required
+          onChange={passwordOnChange}
+          onBlur={passwordOnBlur}
+          name={passwordName}
+          ref={passwordRef}
         />
-      </label>
-      <p className={styles.notice}>
-        Passwords must contain at least eight characters,
-        <br /> including at least 1 letter and 1 number.
-      </p>
-      <button className={styles.signupBtn} type='submit'>
-        Sign up
-      </button>
-      <span>
-        이미 가입하셨나요? <Link to='/signin'>Log in</Link>
-      </span>
-    </form>
-  )
-}
-
-function SignUp() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.formBox}>
-        <Form />
+        <div className={styles.errorWrapper}>
+          {dirtyFields.password && errors.password ? (
+            <span className={styles.error}>{errors.password.message}</span>
+          ) : (
+            <div className={styles.noError} />
+          )}
+        </div>
       </div>
-    </div>
+      <div className={styles.inputBox}>
+        <label className={styles.label} htmlFor='nickname'>
+          이름
+        </label>
+        <input
+          id='nickname'
+          placeholder='앱에서 사용할 이름을 입력해주세요.'
+          className={styles.input}
+          type='text'
+          onChange={nicknameOnChange}
+          onBlur={nicknameOnBlur}
+          name={nicknameName}
+          ref={nicknameRef}
+        />
+        <div className={styles.errorWrapper}>
+          {dirtyFields.nickname && errors.nickname ? (
+            <span className={styles.error}>{errors.nickname.message}</span>
+          ) : (
+            <div className={styles.noError} />
+          )}
+        </div>
+      </div>
+      <TermsOfUse
+        allCheck={allCheck}
+        setAllCheck={setAllCheck}
+        useCheck={useCheck}
+        setUseCheck={setUseCheck}
+        privacyCheck={privacyCheck}
+        setPrivacyCheck={setPrivacyCheck}
+      />
+      <button
+        className={styles.signupBtn}
+        type='submit'
+        disabled={!allCheck || !useCheck || !privacyCheck || Object.keys(errors).length > 0}
+      >
+        회원가입하기
+      </button>
+    </form>
   )
 }
 
