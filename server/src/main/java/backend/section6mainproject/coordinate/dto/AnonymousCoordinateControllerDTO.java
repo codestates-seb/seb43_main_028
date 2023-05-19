3b36319054fdc033df4bb279c01a485f94a65f8b
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-05-19 14:18:42 +0900

    [BE] 비회원 걷기 시작 구현 (#162)
    * Feat: 비회원 걷기 기록 시작 요청 구현 (#159)
    
    * Feat: 비회원 걷기 기록 좌표 전송 구현 (#159)
    
    * Fix: 비회원 걷기 기록 좌표 전송 관련 오류 수정 (#159)

--- server/src/main/java/backend/section6mainproject/coordinate/dto/AnonymousCoordinateControllerDTO.java
+++ server/src/main/java/backend/section6mainproject/coordinate/dto/AnonymousCoordinateControllerDTO.java
@@ -7,21 +7,22 @@ import lombok.Setter;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-public class CoordinateServiceDTO {
+public class AnonymousCoordinateControllerDTO {
     @Getter
     @Setter
-    public static class Input {
-
-        private Long walkLogId;
+    public static class Pub {
+        @NotNull
+        private String userId;
+        @NotNull
         private Double lat;
+        @NotNull
         private Double lng;
     }
 
     @Getter
     @AllArgsConstructor
-    public static class Output {
+    public static class Sub {
         private long coordinateId;
-        private long walkLogId;
         private Double lat;
         private Double lng;
         private LocalDateTime createdAt;
---
+++ server/src/main/java/backend/section6mainproject/walklog/controller/AnonymousWalkLogController.java
@@ -0,0 +1,39 @@
+package backend.section6mainproject.walklog.controller;
+
+import backend.section6mainproject.walklog.dto.AnonymousWalkLogControllerDTO;
+import backend.section6mainproject.walklog.dto.AnonymousWalkLogServiceDTO;
+import backend.section6mainproject.walklog.dto.WalkLogControllerDTO;
+import backend.section6mainproject.walklog.dto.WalkLogServiceDTO;
+import backend.section6mainproject.walklog.service.AnonymousWalkLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/anonymous/walk-logs")
+@Validated
+@RequiredArgsConstructor
+public class AnonymousWalkLogController {
+    private final static String WALK_LOG_DEFAULT_URL = "/anonymous/walk-logs";
+    private final AnonymousWalkLogService walkLogService;
+
+    @PostMapping
+    public ResponseEntity postWalkLog() {
+        AnonymousWalkLogServiceDTO.CreateOutput createOutput = walkLogService.createWalkLog();
+        AnonymousWalkLogControllerDTO.PostResponse postResponse = new AnonymousWalkLogControllerDTO.PostResponse(createOutput.getUserId());
+        URI location = UriComponentsBuilder
+                .newInstance()
+                .path(WALK_LOG_DEFAULT_URL + "/" + createOutput.getUserId())
+                .build()
+                .toUri();
+        return ResponseEntity.created(location).body(postResponse);
+    }
+}
