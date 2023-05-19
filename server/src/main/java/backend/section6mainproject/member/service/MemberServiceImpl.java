4ded9010ccf195d69d95ba347021688a92a6010b
NOTE111 <lolly5488@gmail.com>
2023-05-19 19:50:59 +0900

    Refactor: 비밀번호 수정 및 비밀번호 찾기할때 이메일 유효성 검증 리팩토링 v7 (#147)

--- server/src/main/java/backend/section6mainproject/member/service/MemberServiceImpl.java
+++ server/src/main/java/backend/section6mainproject/member/service/MemberServiceImpl.java
@@ -161,7 +161,7 @@ public class MemberServiceImpl implements MemberService{
     }
 
     private String getTempPassword(int size) {
-    char[] charSet = new char[] {
+    char[] charSet = new char[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             '!', '@', '#', '$', '%', '^', '&' };
 
