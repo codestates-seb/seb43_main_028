0783d4e4f4a2cfa95781a9eca64b4754fddf54ac
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-20 21:05:58 +0900

    [FE] Feat: 랜딩페이지 페이지 구현 (#194)
    * Chore: eslint role 추가 (#153)
    
    * Feat: 지도 풀스크린 모드 해제 (#153)
    
    IOS에서 구글지도의 풀스크린을 지원하지 않음
    풀스크린 모드를 직접 구현할 예정
    
    * Feat: 구글맵 관련 훅에서 옵션을 훅 내부로 옮김 (#153)
    
    구글맵의 타입 설정에서 google을 임포트해오지 못하는 에러가 발생해서 훅 내부로 이동함
    
    * Refactor: 필요없는 돔 nesting 제거(#153)
    
    * Feat: HomeHeader 구현 (#153)
    
    로그인 여부에 따라 2가지로 분기 (로그인, 비로그인)
    
    * Design: 탭바 z-index 수정해서 레이어 안보이는 경우 없게 수정 (#153)
    
    * Feat: MapCanvas 에서 path, position props를 옵셔널로 변경 (#153)
    
    두개가 필요없는 경우가 있음
    
    * Feat: Home 구현 (#153)
    
    홈헤더
    걷기시작버튼 - onwalk로 이동
    지도에서 현재위치 보여줌
    
    * Feat: 걷기시작 요청 (#153)
    
    * Remove: 필요없는 파일삭제 (#153)
    
    * Feat: 임시 로딩 컴포넌트 추가 (#153)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,121 @@
-import { Link } from 'react-router-dom'
-import { useAtom } from 'jotai'
-import { userAtom } from '../store/authAtom'
+import { useState, useEffect } from 'react'
+import { useAtomValue } from 'jotai'
+import { userAtom, isLoginAtom } from '../store/authAtom'
+import useMapRef from '../hooks/useMapRef'
+import MapCanvas from '../components/common/Map/MapCanvas'
+import HomeHeader from '../components/Home/HomeHeader'
+import useRouter from '../hooks/useRouter'
+import styles from './Home.module.scss'
+import { startWalkLog } from '../apis/walkLog'
 
 export default function Home() {
-  // userData는 배열의 형태
-  const [userData, setUserData] = useAtom(userAtom)
+  const { routeTo } = useRouter()
+  const [position, setPosition] = useState<google.maps.LatLngLiteral | null>(null)
+
+  const isLogin = useAtomValue(isLoginAtom)
+  const user = useAtomValue(userAtom)
+  const mapRef = useMapRef()
+
+  let watchId: number
+
+  const userInfo = {
+    nickname: user.nickname,
+    imageUrl: user.imageUrl,
+    totalWalkLog: user.totalWalkLog,
+    totalWalkLogContent: user.totalWalkLogContent,
+  }
+
+  const startWalkLogHandler = async () => {
+    if (!isLogin) return
+    const walkLogId = await startWalkLog({ memberId: user.memberId })
+    if (!walkLogId) return
+    // TODO : walkLogId를 전역으로 저장해서 onwalk 페이지에서 사용해야 한다.
+    // TODO : 로그인/비로그인 ID를 분기해야한다.
+    routeTo('/onwalk')
+  }
+
+  const handleStartClick = () => {
+    startWalkLogHandler()
+  }
+
+  const isSamePosition = (
+    a: google.maps.LatLngLiteral | null,
+    b: google.maps.LatLngLiteral | null
+  ) => a?.lat === b?.lat && a?.lng === b?.lng
+
+  const watchCurrentPosition = () => {
+    watchId = navigator.geolocation.watchPosition(
+      newPosition => {
+        const { latitude, longitude } = newPosition.coords
+        const watchedPosition = { lat: latitude, lng: longitude }
+        if (!isSamePosition(position, watchedPosition)) {
+          setPosition({ lat: latitude, lng: longitude })
+        }
+      },
+      error => console.log(error),
+      { enableHighAccuracy: true, maximumAge: 0, timeout: 5000 }
+    )
+  }
+
+  useEffect(() => {
+    watchCurrentPosition()
+    return () => {
+      navigator.geolocation.clearWatch(watchId)
+    }
+  }, [])
 
   return (
-    <div>
-      <Link to='/maptest'>link to maptest</Link>
-      <p>1</p>
+    <div className={styles.container}>
+      <HomeHeader isLogin={isLogin} userInfo={userInfo} />
+      {position ? (
+        <MapCanvas ref={mapRef} style={{ width: '100%', height: '100dvh' }} position={position} />
+      ) : (
+        // TODO : 로딩 컴포넌트 만들기
+        <div>
+          현위치 찾는 중----------------------------현위치 찾는 중----------------------------현위치
+          찾는 중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------현위치 찾는
+          중----------------------------현위치 찾는 중----------------------------
+        </div>
+      )}
+
+      <button className={styles.startBtn} type='button' onClick={handleStartClick}>
+        걷기 시작
+      </button>
     </div>
   )
 }
