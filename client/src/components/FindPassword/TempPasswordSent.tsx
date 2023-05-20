28897ad8e8dab7daf36ceab52be4a506e629d3d2
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-20 13:15:28 +0900

    [FE] Feat: 비밀번호 찾기 기능 (#175)
    * Feat: 레이아웃 및 기본 로직 구성 (#150)
    
    todo
    - api 연결
    - TempPasswordSent 컴포넌트 레이아웃 구성
    - TempPasswordSent에서 로그인 페이지로 연결
    
    * Feat: 컴포넌트 선택적 렌더링을 위한 state 추가 (#150)
    
    - TempPasswordSent 추가
    
    * Feat: 비밀번호 찾기 기능 구현 (#150)

---
+++ client/src/components/FindPassword/TempPasswordSent.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+import styles from './TempPasswordSent.module.scss'
+
+type TempPasswordSentPropsType = {
+  setIsTempPasswordSent: React.Dispatch<React.SetStateAction<boolean>>
+  userEmail: string
+}
+
+export default function TempPasswordSent({
+  setIsTempPasswordSent,
+  userEmail,
+}: TempPasswordSentPropsType) {
+  const handleClickLinkToLogin = () => {
+    setIsTempPasswordSent(false)
+  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.userEmail}>{userEmail}</div>
+      <div className={styles.confirmTitle}>임시 비밀번호 발급 완료</div>
+      <div className={styles.confirmMessage}>
+        <span>임시 비밀번호를 위 메일로 발송했습니다.</span>
+        <span> 로그인 후 비밀번호를 변경해주세요.</span>
+      </div>
+      <button type='button' className={styles.toLoginPageBtn} onClick={handleClickLinkToLogin}>
+        <Link to='/signin'>로그인 하러가기</Link>
+      </button>
+    </div>
+  )
+}
