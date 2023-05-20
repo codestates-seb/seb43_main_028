3dfe6446ee323b37c58693525eb4e8f0bc5b8571
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-20 21:05:58 +0900

    [FE] Feat: 랜딩페이지 페이지 구현 (#194)
    * Chore: eslint role 추가 (#153)
    
    * Feat: 지도 풀스크린 모드 해제 (#153)
    
    IOS에서 구글지도의 풀스크린을 지원하지 않음
    풀스크린 모드를 직접 구현할 예정
    
    * Feat: 구글맵 관련 훅에서 옵션을 훅 내부로 옮김 (#153)
    
    구글맵의 타입 설정에서 google을 임포트해오지 못하는 에러가 발생해서 훅 내부로 이동함
    
    * Refactor: 필요없는 돔 nesting 제거(#153)
    
    * Feat: HomeHeader 구현 (#153)
    
    로그인 여부에 따라 2가지로 분기 (로그인, 비로그인)
    
    * Design: 탭바 z-index 수정해서 레이어 안보이는 경우 없게 수정 (#153)
    
    * Feat: MapCanvas 에서 path, position props를 옵셔널로 변경 (#153)
    
    두개가 필요없는 경우가 있음
    
    * Feat: Home 구현 (#153)
    
    홈헤더
    걷기시작버튼 - onwalk로 이동
    지도에서 현재위치 보여줌
    
    * Feat: 걷기시작 요청 (#153)
    
    * Remove: 필요없는 파일삭제 (#153)
    
    * Feat: 임시 로딩 컴포넌트 추가 (#153)

--- client/src/hooks/usePolyline.ts
+++ client/src/hooks/usePolyline.ts
@@ -5,22 +5,17 @@ type UsePolylineType = {
   path: google.maps.LatLngLiteral[] | null
 }
 
-const defaultPolylineOptions: google.maps.PolylineOptions = {
-  strokeColor: '#8cff9e',
-  strokeOpacity: 1.0,
-  strokeWeight: 6,
-}
-
 export default function usePolyline({ map, path }: UsePolylineType) {
   const polylineRef = useRef<google.maps.Polyline | null>(null)
 
   useEffect(() => {
-    if (map) {
-      polylineRef.current = new google.maps.Polyline({
-        map,
-        path,
-        ...defaultPolylineOptions,
-      })
-    }
+    if (!path || !map) return
+    polylineRef.current = new google.maps.Polyline({
+      map,
+      path,
+      strokeColor: '#8cff9e',
+      strokeOpacity: 1.0,
+      strokeWeight: 6,
+    })
   }, [map, path])
 }
