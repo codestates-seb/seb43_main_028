c66f001d6ec2505e57c95e3cb494f0d47ca3cead
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-21 22:32:58 +0900

    Feat: 걷기 기록 관련 타입 추가 (#199)

--- client/src/apis/walkLog.ts
+++ client/src/apis/walkLog.ts
@@ -19,4 +19,40 @@ export const startWalkLog = async (memberId: number): Promise<StartWalkLogResTyp
   }
 }
 
+type WalkLogContentType = {
+  walkLogContentId: number
+  text: string
+  createdAt: string
+  imageUrl: string | null
+}
+
+type CoordinateType = {
+  coordinateId: number
+  lat: number
+  lng: number
+  createdAt: string
+}
+
+export type WalkLogType = {
+  walkLogId: number
+  createdAt: string
+  endAt: string
+  memberId: number
+  nickname: string
+  profileImage: string | null
+  message: string
+  mapImage: string | null
+  walkLogStatus: 'RECORDING' | 'STOP'
+  walkLogPublicSetting: 'PRIVATE' | 'PUBLIC'
+  coordinates: CoordinateType[]
+  walkLogContents: WalkLogContentType[]
+}
+
+export const getWalkLog = async (walkLogId: number): Promise<WalkLogType | null> => {
+  try {
+    const { data } = await axiosInstance.get(`/walk-logs/${walkLogId}`)
+    return data as WalkLogType
+  } catch (error: unknown) {
+    return null
+  }
 }
