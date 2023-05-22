45d7a70fc162bdbc74b7fd961ecba4105ef588ec
HeWillGoTillTheEnd <40792205+HeWillGoTillTheEnd@users.noreply.github.com>
2023-05-22 17:44:30 +0900

    216 be feat 우편번호 수신 데이터 region 구현 (#254)
    * Refactor: 기존에 적용 안되어 있던 signedUrl전부 변경 (#216)
    
    * Refactor: SignBucket 추가 (#216)
    
    * Feat: region 데이터 구현 (#216)
    
    * Fix: 오류 발생하는 테스트 코드 변경 (#216)
    
    * Refactor: 잘못 설정된 부분 수정 (#216)
    
    * Refactor: 우편번호 입력관련 정규표현식 추가 (#216)

--- server/src/test/java/backend/section6mainproject/walklog/repository/WalkLogRepositoryTest.java
+++ server/src/test/java/backend/section6mainproject/walklog/repository/WalkLogRepositoryTest.java
@@ -3,19 +3,12 @@ package backend.section6mainproject.walklog.repository;
 import backend.section6mainproject.member.entity.Member;
 import backend.section6mainproject.member.repository.MemberRepository;
 import backend.section6mainproject.walklog.WalkLogStubData;
-import backend.section6mainproject.walklog.entity.WalkLog;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class WalkLogRepositoryTest {
