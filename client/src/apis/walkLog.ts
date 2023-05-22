d7a4531a7270b341ab2fa5769e244f56865bfb55
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-22 13:50:11 +0900

    [FE] Feat: 피드 컴포넌트 구현 (#234)
    * Feat: 피드 구현 시작 (#220)
    
    * Feat: 피드 컴포넌트 구현 (#220)
    
    - 피드 컴포넌트 레이아웃
    - 데이터 배치
    - 무한 스크롤
    
    * Rename: 불필요한 타입 제거 (#220)
    
    * Rename: search-light svg 파일 삭제 (#220)

--- client/src/apis/walkLog.ts
+++ client/src/apis/walkLog.ts
@@ -6,6 +6,17 @@ type StartWalkLogProps = {
   memberId: number
 }
 
+type GetAllPublicWalkLogsParamType = number
+type GetAllPublicWalkLogsResType = {
+  data: []
+  pageInfo: {
+    page: number
+    size: number
+    totalElements: number
+    totalPages: number
+  }
+} | null
+
 export const startWalkLog = async ({
   memberId,
 }: StartWalkLogProps): Promise<StartWalkLogResType> => {
@@ -13,6 +24,17 @@ export const startWalkLog = async ({
     const startWalkLogRes = await axiosInstance.post('/api/walk-logs', { memberId })
     return startWalkLogRes.data.walkLogId
   } catch (error: unknown) {
+    return null
+  }
+}
+
+export const getAllPublicWalkLogs = async (page = 1): Promise<GetAllPublicWalkLogsResType> => {
+  try {
+    const response = await axiosInstance.get('/walk-logs', { params: { page } })
+    const allPublicWalkLogs = response.data
+    return allPublicWalkLogs
+  } catch (error) {
+    console.error(error)
     return null
   }
 }
