1ff2fe3d7de58a694422ac927266ec1dc3483d8a
snow <120469477+iberis2@users.noreply.github.com>
2023-05-22 15:05:10 +0900

    [FE] Fix 걷기기록 한줄메시지 수정하기 에러수정 (#243)
    * Refactor: input값 참조를 useRef → formData로 변경 (#213)
    
    * Fix: 한 줄 메시지 수정시 바로 반영 안되는 것 수정 (#213)
    
    부모 컴포넌츠에서 받은 데이터를 state로 사용해서 업데이트가 한 박자 늦음
    → useEffect 안에 state를 넣어서 해결
    
    * Fix: 이미지 수정시 x 누르면 preview 바로 삭제 (#213)
    
    preview의 initial value 가 있을 때 x 누르면 사진이 바로 삭제되지 않고 initial value 가 나오고,
    그 initial value preview 에서 x 해야 사진이 삭제 되는 문제발생
    ! imgFile 일때 setPreview(initialValue)여서 생기는 문제
    → x 누르면preview를 없애고 setPreview(''),
    useEffect에서 (! imgFile && ! preview)일 때 preview 없어지고 setPreview(''),
    ! imgFile일 땐 setPreview(initialValue)로 설정
    
    * Fix: imageFile 변경될 때만 contentImage 수정 전송 (#213)
    
    이미지가 있는 상태에서 수정할 때, text만 수정하고 전송해도 contentImage도 공백으로 수정 요청 보내짐
    image 를 formDate.get('image')로 가져와서,
    if(image)일 때만 patch body에 contentImage를 넣었는데,
    image file을 선택 안해도 formData엔 항상 길이 0인 File 객체로 존재서 생긴 문제
    → if(image.name)일 때만 contentImage도 함께 전송하도록 수정
    - 파일이 안들어 있으면 File 객체의 name은 공백('')
    
    * Refactor: dropDown 컴포넌츠 안으로 로직 넣음 (#213)
    
    현재 public setting과 onSubmit 함수만 drop down 컴포넌츠로 내려주면
    현재  public setting을 title로 보여주고,
    옵션 선택시 서버로 전송하고 선택 된 옵션으로 보여지는 로직을 drop down 컴포넌츠 안에서 모두 처리하도록 수정 (#213)
    
    * Fix: DropDown 타입 에러 수정 (#213)
    
    {  PUBLIC: '전체 공개',  PRIVATE: '나만 보기' }에서 value로 key를 찾기 위해
    Object.keys(obj).find(key => obj[key] === '한글 옵션') 로직에서
    key를 객체의 key가 아닌 string 타입으로 인식해서 생기는 에러 고치기 위해
    const obj: { [key: string]: '전체 공개' | '나만 보기' }
    = {  PUBLIC: '전체 공개',  PRIVATE: '나만 보기' }
    객체 타입의 key를 지정해줌
    
    * Feat: 로그인 후 fileAxios 로 보낼 때 헤더에 토큰 담기 추가 (#213)
    
    * Feat: 순간기록 이미지가 있을 때, 텍스트만 수정, 이미지만 삭제 나눔 (#213)
    
    순간기록 이미지가 있는 상태에서 수정 요청 할 때,
    - 이미지 교체 없이 텍스트만 수정하면,
    patch 요청 바디에 "contentImage" 빠지고
    {content {text: "수정내용"}} 으로 보냄
    - 이미지 삭제하면
    patch 요청 바디
    {
    content {text: "수정내용"},
    contentImage: file.name=""
    }

--- client/src/components/common/DropDown.tsx
+++ client/src/components/common/DropDown.tsx
@@ -1,60 +1,65 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './DropDown.module.scss'
 import Icon from './Icon'
 
 type DropDownProps = {
-  options: {
-    id: number
-    title: string
-    handleClick: (title: string) => void
-    param: string
-  }[]
+  currentSetting: 'PUBLIC' | 'PRIVATE'
+  onSubmit: (option: string) => void
 }
 
-type OptionType = {
-  id: number
-  title: string
-  handleClick: (title: string) => void
-  param: string
-}
+type OptionType = '전체 공개' | '나만 보기'
 
-function DropDown({ options }: DropDownProps) {
-  const [isOptionOpened, setIsOptionOpened] = useState(false)
-  const [selectedOptionTitle, setSelectedOptionTitle] = useState(options[0].title)
+const options: OptionType[] = ['전체 공개', '나만 보기']
+const engOptionsObj: { [key: string]: OptionType } = {
+  PUBLIC: '전체 공개',
+  PRIVATE: '나만 보기',
+}
+function DropDown({ currentSetting, onSubmit }: DropDownProps) {
+  const [isOptionOpened, setIsOptionOpened] = useState<boolean>(false)
+  const [selectedOptionTitle, setSelectedOptionTitle] = useState<OptionType>(
+    engOptionsObj[currentSetting]
+  )
 
   const handleChangeCurrentOption = (option: OptionType) => {
-    setSelectedOptionTitle(option.title)
-    setIsOptionOpened(prev => !prev)
-    option.handleClick(option.param)
+    setSelectedOptionTitle(option)
+    setIsOptionOpened(false)
+
+    const engOption = Object.keys(engOptionsObj).find(key => engOptionsObj[key] === option)
+
+    if (engOption) onSubmit(engOption)
   }
+
   const handleOpenOptions = () => {
     setIsOptionOpened(prev => !prev)
   }
+
+  useEffect(() => {
+    setSelectedOptionTitle(engOptionsObj[currentSetting])
+  }, [currentSetting])
+
   return (
     <ul className={styles.listContainer}>
-      <div className={`${styles.listTitle} ${isOptionOpened ? styles.open : ''}`}>
-        <button type='button' onClick={handleOpenOptions}>
-          {selectedOptionTitle}
-        </button>
-        <div className={styles.buttonWrapper}>
-          <button className={styles.openBtn} type='button' onClick={handleOpenOptions}>
-            <Icon name='arrow-drop-down' size={16} />
-          </button>
-        </div>
-      </div>
-      {isOptionOpened ? (
+      <button
+        className={isOptionOpened ? styles.open : styles.listTitle}
+        type='button'
+        onClick={handleOpenOptions}
+      >
+        <p>{selectedOptionTitle}</p>
+        <Icon name='arrow-drop-down' size={16} />
+      </button>
+      {isOptionOpened && (
         <div className={styles.listBox}>
           {options.map(option => {
             return (
-              <li key={option.id}>
+              <li key={crypto.randomUUID()}>
                 <button type='button' onClick={() => handleChangeCurrentOption(option)}>
-                  {option.title}
+                  {option}
                 </button>
               </li>
             )
           })}
         </div>
-      ) : null}
+      )}
     </ul>
   )
 }
