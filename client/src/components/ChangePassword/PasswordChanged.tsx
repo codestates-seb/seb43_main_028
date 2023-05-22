82471a270d04494312803d47da113142600b49a5
hajongon <hajongon@gmail.com>
2023-05-22 20:28:30 +0900

    Feat: 버그 수정 (#269)
    - 비밀번호 변경 후 로그아웃되지 않는 이슈 해결
    - 피드 당일 게시글의 경우 '0일 전'에서 'X시간 전', 'X분 전', '방금 전'으로 createdAt 표시 변경
    - 이메일, 비밀번호, 닉네임 등을 입력하는 input 박스 border-bottom 끝이 둥글게 보이는 문제 해결

--- client/src/components/ChangePassword/PasswordChanged.tsx
+++ client/src/components/ChangePassword/PasswordChanged.tsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom'
 import { useAtom } from 'jotai'
 import { isLoginAtom } from '../../store/authAtom'
 import styles from './PasswordChanged.module.scss'
+import { logoutUser } from '../../apis/user'
+import useRouter from '../../hooks/useRouter'
 
 type PasswordChangedPropsType = {
   setIsChangingPassword: React.Dispatch<React.SetStateAction<boolean>>
@@ -13,18 +15,20 @@ export default function PasswordChanged({
   setIsPasswordChanged,
 }: PasswordChangedPropsType) {
   const [, setIsLogin] = useAtom(isLoginAtom)
-  const handleLogout = () => {
-    console.log('handleLogout')
+  const { routeTo } = useRouter()
+  const handleLogout = async () => {
+    await logoutUser()
     setIsLogin(false)
     setIsChangingPassword(false)
     setIsPasswordChanged(false)
+    routeTo('/signin')
   }
   return (
     <div className={styles.container}>
       <div className={styles.confirmTitle}>비밀번호 변경완료</div>
       <div className={styles.confirmMessage}>변경하신 비밀번호로 다시 로그인해주세요.</div>
       <button type='button' className={styles.toLoginPageBtn} onClick={handleLogout}>
-        <Link to='/signin'>로그인 하러가기</Link>
+        <span>로그인 하러가기</span>
       </button>
     </div>
   )
