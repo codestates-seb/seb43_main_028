9d1e55e0865bb4c4b20073d21abb406e22ffd84d
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-22 19:43:47 +0900

    [FE] Fix: 배포 시 발생하는 버그 수정 (#263)
    * Fix: 로그인 에러 분기 처리 (#253)
    
    - 이메일 중복, 닉네임 중복 분기 처리
    
    * Fix: signup unknown 에러 분기 처리 (#253)
    
    * Fix: 로그인 시 에러 분기 처리 (#253)
    
    - 가입되지 않은 이메일이거나 잘못된 비밀번호 일 경우 401 에러
    - 나머지 에러에 대해서는 '잠시 후 다시 시도해주세요.' 메시지 alert
    
    * Fix: 마이페이지 버그 수정 (#253)
    
    - profile edit 닉네임 중복 시 edit 창 닫히는 이슈 해결
    - 비로그인 상태로 마이페이지 접근 시 에러 메시지 출력 후 리다이렉트되는 이슈 해결
    
    * Fix: 엔그로그 서버 가리기
    
    ---------
    
    Co-authored-by: Sangbeom Heo <sangbeomheoo@gmail.com>

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -20,7 +20,7 @@ function SignIn() {
     const email = formData.get('email')
     const password = formData.get('password')
 
-    const { memberId } = await signIn({ email, password, autoLogin: true })
+    const { status, memberId } = await signIn({ email, password, autoLogin: true })
 
     if (memberId) {
       setId(memberId)
@@ -31,9 +31,9 @@ function SignIn() {
       routeTo('/')
       return
     }
-    alert('Sorry, you failed to log in.')
+    if (status === 'invalid-info') alert('가입되지 않은 이메일이거나 잘못된 비밀번호입니다.')
+    if (status === 'unknown-error') alert('잠시 후 다시 시도해주세요.')
   }
-  console.log(user)
 
   return (
     <>
