87dac469233a454c1776407e8995f1fdeb91c759
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-05-22 20:31:00 +0900

    256 be fix 쿠키 조회 안되는 문제 수정 (#271)
    * Fix: 쿠키 설정 변경 (#256)
    
    * Fix: 쿠키 설정 변경 (#256)
    
    * Fix: 쿠키 설정 변경 (#256)
    
    * Fix: 쿠키 설정 변경 (#256)
    
    * Fix: 쿠키 설정 변경 (#256)
    
    * Fix: CORS 설정 변경 (#256)
    
    * Fix: CORS 설정 변경 (#256)

--- server/src/main/java/backend/section6mainproject/auth/SecurityConfiguration.java
+++ server/src/main/java/backend/section6mainproject/auth/SecurityConfiguration.java
@@ -89,7 +89,7 @@ public class SecurityConfiguration {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(List.of("https://www.would-you-walk.com"));
-        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Set-Cookie"));
         corsConfiguration.setExposedHeaders(List.of("Authorization", "Refresh"));
         corsConfiguration.setAllowedMethods(List.of("POST", "GET", "PATCH", "DELETE", "OPTIONS"));
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
