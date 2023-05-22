7001f43f2dcbad5981e31c8d535875ab5e036350
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-23 01:02:20 +0900

    Feat: Afterwalk 걷고있는 기록 정보 불러오기 (#280)

--- client/src/pages/AfterWalk.tsx
+++ client/src/pages/AfterWalk.tsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import useRouter from '../hooks/useRouter'
+import { getWalkLog, WalkLogType, WalkLogContentType } from '../apis/walkLog'
+
 export default function AfterWalk() {
+  const { routeTo } = useRouter()
+  const { id: walkLogId } = useParams()
+
+  const [walkLog, setWalkLog] = useState<WalkLogType | null>(null)
+  const [snaps, setSnaps] = useState<WalkLogContentType[]>(walkLog?.walkLogContents || [])
+
+  const getWalkLogData = async () => {
+    const data = await getWalkLog(Number(walkLogId))
+    if (data?.walkLogStatus === 'STOP') {
+      routeTo('/')
+      return
+    }
+    if (data) {
+      setWalkLog(data)
+      setSnaps(data.walkLogContents)
+    }
+  }
+
+  // useEffect(() => {
+  //   getWalkLogData()
+  // }, [])
+
   return <div>AfterWalk</div>
 }
