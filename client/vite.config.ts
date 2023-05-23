4b635e4b9bdf990caddcd17d05a29c98a8445d9f
hajongon <hajongon@gmail.com>
2023-05-23 20:18:24 +0900

    Design: image 파일 비율 깨짐 이슈 해결 (#315)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: '',
+        target: 'https://32bc-49-171-175-238.ngrok-free.app',
         changeOrigin: true,
         secure: false,
         rewrite: path => path.replace(/^\/api/, ''),
