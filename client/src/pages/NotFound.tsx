94951639e533cdd535fbf567a6973ebbb3556062
snow <120469477+iberis2@users.noreply.github.com>
2023-05-23 13:39:51 +0900

    [FE] Design: skeleton css 로딩 화면 & notFound Page 적용 (#286)
    * Design: skeleton loading 화면 만듦 (#274)
    
    AfterWalk, OnWalk, Home, HistoryList, HistoryDetail, History, MyPage,
    Feed
    
    * Feat: 각 컴포넌츠, suspense 에 skeleton css 적용 (#274)
    
    * Refactor: 빼먹은 커밋 추가 (#274)
    
    HistoryList에서 안쓰는 OnWalkLoading, AfterWalkLoading import 삭제
    index.tsx에서 안쓰는 Suspense 삭제
    - routerData 에서 각 페이지별로 Suspense 사용
    
    * Design: 사용자 체크 로딩 중일 때 스피너로 보여 줌 (#274)
    
    * Refactor: 안쓰는데 선언된 user 변수 삭제 (#274)
    
    * Design: notFound 페이지 css (#274)

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,3 +1,35 @@
+import { Link } from 'react-router-dom'
+import styles from './NotFound.module.scss'
+import useRouter from '../hooks/useRouter'
+
 export default function NotFound() {
-  return <div>NotFound</div>
+  const { routeTo } = useRouter()
+  const handleGoBack = () => {
+    routeTo(-1)
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.inner}>
+        <p className={styles.title}>페이지를 찾을 수 없습니다</p>
+        <p className={styles.content}>
+          입력하신 페이지의 주소가 잘못 되었거나,
+          <br />
+          주소의 변경 혹은 삭제로 인해
+          <br />
+          현재 페이지를 사용하실 수 없습니다.
+          <br />
+          주소가 정확한지 다시 한번 확인해 주세요.
+        </p>
+        <div className={styles.btnBox}>
+          <Link className={styles.link} to='/'>
+            홈으로
+          </Link>
+          <button onClick={handleGoBack} type='button' className={styles.goBackBtn}>
+            이전 페이지
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
