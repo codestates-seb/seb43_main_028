46622742da75ed943d7c747db5aa1c9ff592271e
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-23 21:15:32 +0900

    [FE] Design: image 파일 비율 깨짐 이슈 해결 (#318)
    * Design: image 파일 비율 깨짐 이슈 해결 (#315)
    
    * Rename: vite.config.ts proxy target 삭제 (#315)

--- client/src/components/HistoryList/History.tsx
+++ client/src/components/HistoryList/History.tsx
@@ -24,7 +24,9 @@ export default forwardRef(function History({ data }: HistoryItemProps, ref: Ref<
   const body = (
     <li className={styles.container}>
       <div className={styles.mapTimeBox}>
-        <img src={mapImage} className={styles.map} alt='지도 이미지' />
+        <div className={styles.imgWrapper}>
+          <img src={mapImage} className={styles.map} alt='지도 이미지' />
+        </div>
         <div>
           <p className={styles.date}>{format(new Date(startedAt), 'yyyy년 M월 d일')}</p>
           <div className={styles.timeBox}>
