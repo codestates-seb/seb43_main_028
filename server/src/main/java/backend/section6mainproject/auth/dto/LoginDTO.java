a5f98e394bc298bdaa485a325e7a42d47fc40adc
kim-ksp7331 <119999208+kim-ksp7331@users.noreply.github.com>
2023-05-24 10:16:09 +0900

    299 be test 인증 관련 테스트 (#336)
    * Test: 로그인 테스트 코드 추가 (#299)
    
    * Feat: 로그인 확인 기능 추가 (#299)

--- server/src/main/java/backend/section6mainproject/auth/dto/LoginDTO.java
+++ server/src/main/java/backend/section6mainproject/auth/dto/LoginDTO.java
@@ -1,10 +1,12 @@
 package backend.section6mainproject.auth.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Setter
 public class LoginDTO {
     @NotBlank
     private String email;
