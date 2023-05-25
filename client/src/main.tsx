9c3b07c18bca9f48ec66bfba6cecc37caec6240a
snow <120469477+iberis2@users.noreply.github.com>
2023-05-26 00:25:30 +0900

    [FE] Fix: 순간 기록 수정 삭제 바로 반영 안되는 문제 (#361)
    * test: 순간기록 수정/삭제 바로 안되는 것 확인용 (#359)
    
    onSuccess 에 있는 함수 실행되는 지 확인
    
    * Fix: 구글 맵api와 충돌해서 react query 동작 안하는 것 고침 (#359)
    
    <QueryClientProvider>는 Main 컴포넌츠에서 App을 감싸고,
    <GoogleMapsProvider>와 <MapRefContext.Provider>는 App 컴포넌츠 안에서
    하위 children들을 감싸도록 바꿈
    
    * Formatting: 한 줄 빈 공백 지움 (#359)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>
 )
