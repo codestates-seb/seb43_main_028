38e55fb0b94fdf2a611a489005146829baeec1a6
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-25 15:11:08 +0900

    Feat: 웹소캣 연결 전까지 저장된 경로를 불러올 때 더미 경로를 불러옴 (#348)

--- client/src/components/common/Map/StaticPathMap.tsx
+++ client/src/components/common/Map/StaticPathMap.tsx
@@ -18,30 +18,28 @@ type StaticPathMapProps = {
   path?: google.maps.LatLngLiteral[] | null
 }
 
-export const StaticPathMap = forwardRef<HTMLDivElement, StaticPathMapProps>(
-  ({ path = null }, ref) => {
-    const [isFullScreenMode, setIsFullScreenMode] = useState<boolean>(false)
+const StaticPathMap = forwardRef<HTMLDivElement, StaticPathMapProps>(({ path = null }, ref) => {
+  const [isFullScreenMode, setIsFullScreenMode] = useState<boolean>(false)
 
-    const startAt = path ? path[0] : null
-    const endAt = path ? path[path.length - 1] : null
+  const startAt = path ? path[0] : null
+  const endAt = path ? path[path.length - 1] : null
 
-    const map = useGoogleMap() || null
-    useMarker({ map, position: startAt, icon: stopMarkerIcon })
-    useMarker({ map, position: endAt, icon: stopMarkerIcon })
-    usePolyline({ map, path })
+  const map = useGoogleMap() || null
+  useMarker({ map, position: startAt, icon: stopMarkerIcon })
+  useMarker({ map, position: endAt, icon: stopMarkerIcon })
+  usePolyline({ map, path })
 
-    const toggleScreenSize = () => setIsFullScreenMode(prev => !prev)
+  const toggleScreenSize = () => setIsFullScreenMode(prev => !prev)
 
-    return (
-      <div className={isFullScreenMode ? styles.fullScreen : styles.basicMapSize}>
-        <div className={styles.mapRef} ref={ref} />
-        <div className={styles.btnWrapper}>
-          <MapButton name={isFullScreenMode ? 'reduce' : 'full'} handleClick={toggleScreenSize} />
-        </div>
+  return (
+    <div className={isFullScreenMode ? styles.fullScreen : styles.basicMapSize}>
+      <div className={styles.mapRef} ref={ref} />
+      <div className={styles.btnWrapper}>
+        <MapButton name={isFullScreenMode ? 'reduce' : 'full'} handleClick={toggleScreenSize} />
       </div>
-    )
-  }
-)
+    </div>
+  )
+})
 
 StaticPathMap.displayName = 'StaticPathMap'
 
--- client/src/pages/OnWalk.tsx
+++ client/src/pages/OnWalk.tsx
@@ -14,19 +14,7 @@ import LiveMap, { MapSize } from '../components/common/Map/LiveMap'
 import styles from './OnWalk.module.scss'
 import { getDistanceBetweenPosition } from '../utils/position'
 
-const coordinates = [
-  { lat: 37.5747076, lng: 127.0459042 }, // 전농동
-  { lat: 37.5789, lng: 126.9436 },
-  { lat: 37.582, lng: 126.9518 },
-  { lat: 37.5842, lng: 126.9591 },
-  { lat: 37.5906, lng: 126.9589 },
-  { lat: 37.5939, lng: 126.9534 },
-  { lat: 37.5951, lng: 126.9514 },
-  { lat: 37.5989, lng: 126.9496 }, // 서울시립대학교
-]
-
 export default function OnWalk() {
-  console.log(coordinates)
   const { routeTo } = useRouter()
   const { id: walkLogId } = useParams()
 
