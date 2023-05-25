81aefe36f4ec6418047ddb0f6cb3c4aab92ecd48
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-25 15:50:36 +0900

    [FE] Feat: 걷기 중 실시간 경로 렌더링 / 정적 경로렌더링 구현 (#355)
    * Refactor: 좌표, 경로 전부 추적하는 코드 추가 (#348)
    
    * Feat: 두 좌표사이의 거리 비교를 최대 2m로 설정 (#348)
    
    * Feat: onWalk 내부 맵 스타일 조정 (#348)
    
    * Refactor: 현재위치와 걸은 경로를 하나의 state로 관리하도록 변경 (#348)
    
    * Refactor: 맵 종류를 static, live 2가지로 나눔 (#348)
    
    - 현재 위치를 계속 받오는지 차이
    
    * Feat: 웹소캣 연결 전까지 저장된 경로를 불러올 때 더미 경로를 불러옴 (#348)
    
    * Feat: 맵의 Basic 사이즈를 디폴트로 설정 (#348)
    
    * Feat: StaticMap 일 경우 경로의 중간에 맵이 위치 (#348)

---
+++ client/src/components/common/Map/StaticPathMap.tsx
@@ -0,0 +1,55 @@
+import { forwardRef, useState } from 'react'
+import { useGoogleMap } from '@ubilabs/google-maps-react-hooks'
+import useMarker from '../../../hooks/useMarker'
+import usePolyline from '../../../hooks/usePolyline'
+import { MapButton } from './MapButton'
+import styles from './Map.module.scss'
+import { MapSize } from './LiveMap'
+
+const stopMarkerIcon = {
+  path: google.maps.SymbolPath.CIRCLE,
+  scale: 8,
+  fillColor: 'white',
+  fillOpacity: 1,
+  strokeWeight: 1,
+  strokeColor: '#212121',
+}
+
+type StaticPathMapProps = {
+  mapSize?: MapSize
+  path?: google.maps.LatLngLiteral[] | null
+}
+
+const StaticPathMap = forwardRef<HTMLDivElement, StaticPathMapProps>(
+  ({ mapSize = MapSize.BASIC, path = null }, ref) => {
+    const [isFullScreenMode, setIsFullScreenMode] = useState<boolean>(false)
+
+    const startAt = path ? path[0] : null
+    const endAt = path ? path[path.length - 1] : null
+    const middleAt = path ? path[Math.floor(path.length / 2)] : null
+
+    const map = useGoogleMap() || null
+
+    map?.setZoom(14)
+    middleAt && map?.setCenter(middleAt)
+
+    useMarker({ map, position: startAt, icon: stopMarkerIcon })
+    useMarker({ map, position: endAt, icon: stopMarkerIcon })
+    usePolyline({ map, path })
+
+    const toggleScreenSize = () => setIsFullScreenMode(prev => !prev)
+
+    return (
+      <div className={isFullScreenMode ? styles.fullScreen : styles[mapSize]}>
+        <div className={styles.mapRef} ref={ref} />
+        <div className={styles.btnWrapper}>
+          <MapButton name={isFullScreenMode ? 'reduce' : 'full'} handleClick={toggleScreenSize} />
+        </div>
+      </div>
+    )
+  }
+)
+
+StaticPathMap.displayName = 'StaticPathMap'
+
+export default StaticPathMap
