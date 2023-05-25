724ead58356ac50a18d82c55dbe1b530fa85a657
hajongon <114554291+hajongon@users.noreply.github.com>
2023-05-25 23:58:17 +0900

    [FE] Design: 앱 아이콘 및 파비콘 추가 (#360)
    * Feat: 아이콘 추가 및 적용 (#346) (#347)
    
    * Design: favicon 및 앱 아이콘 테스트 (#351) (#352)
    
    * Test: 파일 이름 수정 (#353)
    
    * Design: icon favicon added (#353)
    
    ---------
    
    Co-authored-by: Sangbeom Heo <sangbeomheoo@gmail.com>

--- client/src/pages/loadingPage/Spinner.tsx
+++ client/src/pages/loadingPage/Spinner.tsx
@@ -1,14 +1,18 @@
 import styles from './Spinner.module.scss'
+import characterIcon from '../../assets/icon_no_bg_180.png'
 
 export default function Spinner() {
   return (
     <div className={styles.box}>
-      <div className={styles.ldsRing}>
-        <div />
-        <div />
-        <div />
-        <div />
+      <div className={styles.imgWrapper}>
+        <img src={characterIcon} alt='loading' />
       </div>
+      {/* <div className={styles.ldsRing}>
+        <div />
+        <div />
+        <div />
+        <div />
+      </div> */}
       <p>
         <span className={styles.label}>사용자 체크 중...</span>
       </p>
