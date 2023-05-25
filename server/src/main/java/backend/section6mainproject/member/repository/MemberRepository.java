08c61327ff795ef33b921bc54f631239a7f91ea0
Leejh <97214060+NOTE111@users.noreply.github.com>
2023-05-25 10:43:54 +0900

    Feat: 회원탈퇴시 WYWdel + 숫자 패턴의 닉네임으로 변경 및 회원탈퇴시점 90일 경과 후 자동삭제 로직 구현 (#331) (#343)
    * Feat: 회원탈퇴시 WYWdel + 숫자 패턴의 닉네임으로 변경 및 회원탈퇴시점 90일 경과 후 자동삭제 로직 구현 (#331)
    
    * Fix: Test에서 alter table add column quittedAt에서 안 넘어가는 문제 트러블슈팅 (#342)
    
    * Fix: for Test (#342)

--- server/src/main/java/backend/section6mainproject/member/repository/MemberRepository.java
+++ server/src/main/java/backend/section6mainproject/member/repository/MemberRepository.java
@@ -2,8 +2,10 @@ package backend.section6mainproject.member.repository;
 
 import backend.section6mainproject.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -18,5 +20,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query(value = "SELECT * FROM member WHERE nickname = ?", nativeQuery = true)
     Optional<Member> findByNicknameAll(String nickname);
 
-
+    @Modifying
+    @Query("DELETE FROM Member m WHERE m.memberStatus = 'MEMBER_QUIT' AND m.updatedAt <= :cutoffDateTime")
+    void deleteMemberCompletely(LocalDateTime cutoffDateTime);
 }
