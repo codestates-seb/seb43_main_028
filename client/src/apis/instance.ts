53529cf4bc831f6cd51c12eced3703bba8e0f6ed
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-29 07:02:02 +0900

    Refactor: instance 3가지로 구분해서 작성 (#365)
    - 기본
    - 파일
    - 토큰

--- client/src/apis/instance.ts
+++ client/src/apis/instance.ts
@@ -1,65 +1,92 @@
-import axios, { AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios'
+/* eslint-disable no-param-reassign */
+import axios from 'axios'
 import { getAccessTokenFromLocalStorage } from '../utils/accessTokenHandler'
 
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_ENDPOINT,
-  timeout: 3000,
-  headers: {
-    Authorization: getAccessTokenFromLocalStorage(),
-    'Content-Type': 'application/json',
-  },
-  withCredentials: true,
-})
+const axiosInstance = createAxiosInstance()
+const fileAxios = createFileAxiosInstance()
+const authInstance = createAuthAxiosInstance()
 
-export const fileAxios = axios.create({
-  baseURL: import.meta.env.VITE_API_ENDPOINT,
-  timeout: 3000,
-  headers: {
-    Authorization: getAccessTokenFromLocalStorage(),
-    'Content-Type': 'multipart/form-data',
-  },
-  withCredentials: true,
-})
+function createAxiosInstance() {
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_ENDPOINT,
+    timeout: 3000,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+  })
 
-axiosInstance.interceptors.response.use(
-  config => {
+  instance.interceptors.request.use(config => {
     const accessToken = getAccessTokenFromLocalStorage()
-    // eslint-disable-next-line no-param-reassign
-    config.headers = {
-      Authorization: accessToken || '',
-    }
+    console.log('그냥인스턴스')
+    config.headers.Authorization = accessToken || ''
     return config
-  },
-  error => {
-    return Promise.reject(error)
-  }
-)
+  })
 
-export const authInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_ENDPOINT,
-  timeout: 3000,
-  headers: {
-    Authorization: getAccessTokenFromLocalStorage(),
-    'Content-Type': 'application/json',
-  },
-  withCredentials: true,
-})
+  return instance
+}
 
-authInstance.interceptors.response.use(
-  response => response,
-  async error => {
-    const {
-      config,
-      response: { status },
-    } = error
+function createFileAxiosInstance() {
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_ENDPOINT,
+    timeout: 3000,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    withCredentials: true,
+  })
 
-    if (status === 401) {
-      const originalRequest = config
-      const refreshRes = await axiosInstance.get('/members/refresh')
-      if (refreshRes.status === 200) {
-        return axios(originalRequest)
+  instance.interceptors.request.use(config => {
+    const accessToken = getAccessTokenFromLocalStorage()
+    config.headers.Authorization = accessToken || ''
+    return config
+  })
+
+  return instance
+}
+
+function createAuthAxiosInstance() {
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_ENDPOINT,
+    timeout: 3000,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+  })
+
+  instance.interceptors.request.use(config => {
+    const accessToken = getAccessTokenFromLocalStorage()
+    console.log('어쓰인스턴스')
+    config.headers.Authorization = accessToken || ''
+    return config
+  })
+
+  instance.interceptors.response.use(
+    response => response,
+    async error => {
+      const {
+        config,
+        response: { status },
+      } = error
+
+      if (status === 401) {
+        const originalRequest = config
+        try {
+          const refreshRes = await axiosInstance.get('/members/refresh')
+          if (refreshRes.status === 200) {
+            return await axios(originalRequest)
+          }
+        } catch (error) {
+          // Handle refresh token failure
+        }
       }
+
+      return Promise.reject(error)
     }
-    return Promise.reject(error)
-  }
-)
+  )
+
+  return instance
+}
+
+export { axiosInstance, fileAxios, authInstance }
