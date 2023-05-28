f86b7dc386b5782df29a5aa25aeba16936d40d6d
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-29 08:16:37 +0900

    Refactor: authHandler 리팩토링 (#365)
    - userInfo의 값으로 로그인여부 판단
    - userInfo만 설정

--- client/src/layout/GeneralLayout.tsx
+++ client/src/layout/GeneralLayout.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useSetAtom } from 'jotai'
-// import useRouter from '../hooks/useRouter'
+import useRouter from '../hooks/useRouter'
 import { getCurrentUserInfo } from '../apis/user'
 import { userInfoAtom } from '../store/authAtom'
 import { TapBarContent } from '../router/routerData'
@@ -14,56 +14,27 @@ type GeneralLayoutProps = {
 }
 
 export default function GeneralLayout({ children, showTapBar, withAuth }: GeneralLayoutProps) {
-  console.log('withAuth', withAuth)
+  const { pathname, routeTo } = useRouter()
 
   const [isAuthChecking, setIsAuthChecking] = useState(true)
   const setUserInfo = useSetAtom(userInfoAtom)
-  // const { routeTo, pathname } = useRouter()
 
   const authHandler = async () => {
     const userInfo = await getCurrentUserInfo()
     setUserInfo(userInfo)
-    setIsAuthChecking(false)
+
+    if (!userInfo && withAuth) {
+      routeTo('/signin')
+    } else if (userInfo && (pathname === '/signin' || pathname === '/signup')) {
+      routeTo('/')
+    } else {
+      setIsAuthChecking(false)
+    }
   }
 
   useEffect(() => {
-    console.log('app useEffect 실행')
     authHandler()
-  }, [])
-  // useEffect(() => {
-  // console.log('app useEffect 실행')
-  // authHandler().then(() => {
-  //   setIsAuthChecking(false)
-  // })
-  // const authHandler = async () => {
-  //   const { userInfo } = await getUserInfo()
-  //   if (userInfo === null) {
-  //     const isRefreshed = await refreshAccessToken()
-  //     if (isRefreshed === 'success') {
-  //       const { userInfo } = await getUserInfo()
-  //       if (userInfo) {
-  //         setIsLogin(true)
-  //         setId(userInfo.memberId)
-  //         setUser(userInfo)
-  //         return 'login'
-  //       }
-  //     } else {
-  //       setIsLogin(false)
-  //       return 'logout'
-  //     }
-  //   } else {
-  //     setIsLogin(true)
-  //     setId(userInfo.memberId)
-  //     setUser(userInfo)
-  //     return 'login'
-  //   }
-  // }
-  // authHandler().then(loginStatus => {
-  //   setIsAuthChecking(false)
-  //   if (!isLogin && withAuth && loginStatus === 'logout') routeTo('/signin')
-  //   if (isLogin && (pathname === '/signin' || pathname === '/signup')) routeTo('/')
-  // })
-  // }, [])
+  }, [pathname])
 
   if (isAuthChecking) return <Spinner />
 
