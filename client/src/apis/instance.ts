9d4e4674a1dc8d016050c27cc8f9d278b4520311
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-29 05:34:05 +0900

    Feat: authInstance 생성 (#365)
    - 엑세스토큰, 리프레시토큰 갱신 요청에 사용할 인스턴스 생성
    - 엑세스토큰 만료 시 리프레시토큰 요청되게 구현

--- client/src/apis/instance.ts
+++ client/src/apis/instance.ts
@@ -21,3 +21,31 @@ export const fileAxios = axios.create({
   withCredentials: true,
 })
 
+export const authInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_ENDPOINT,
+  timeout: 3000,
+  headers: {
+    Authorization: getAccessTokenFromLocalStorage(),
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+})
+
+authInstance.interceptors.response.use(
+  response => response,
+  async error => {
+    const {
+      config,
+      response: { status },
+    } = error
+
+    if (status === 401) {
+      const originalRequest = config
+      const refreshRes = await axiosInstance.get('/members/refresh')
+      if (refreshRes.status === 200) {
+        return axios(originalRequest)
+      }
+    }
+    return Promise.reject(error)
+  }
+)
