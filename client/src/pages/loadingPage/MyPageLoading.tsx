7b3ccf0c5f735b50c75ea5461885f7041209acc0
iberis2 <snow9402@gmail.com>
2023-05-30 23:01:46 +0900

    Design: MyPageLoading 에서 text 뺌 (#386)

--- client/src/pages/loadingPage/MyPageLoading.tsx
+++ client/src/pages/loadingPage/MyPageLoading.tsx
@@ -7,7 +7,7 @@ function ProfileBox() {
       <ContentLoader
         speed={2}
         width='100%'
-        height='75%'
+        height='162'
         backgroundColor='#e3e3e3'
         foregroundColor='#ecebeb'
       >
@@ -15,8 +15,8 @@ function ProfileBox() {
         <circle cx='80%' cy='23%' r='32' width='150' height='150' />
         <rect x='0' y='60' rx='3' ry='3' width='100' height='12' />
         <rect x='0' y='80' rx='3' ry='3' width='120' height='12' />
+        <rect x='0' y='132' rx='3' ry='3' width='100%' height='30' />
       </ContentLoader>
-      <p className={styles.editProfileBtn}>프로필 수정하기</p>
     </div>
   )
 }
@@ -26,24 +26,32 @@ export default function MyPageLoading() {
     <div className={styles.container}>
       <ProfileBox />
       <div className={styles.configBox}>
-        <p className={styles.configTitle}>걷기 기록 공개 설정</p>
         <ContentLoader
           speed={2}
           width='100%'
-          height='72'
+          height='135'
           backgroundColor='#e3e3e3'
           foregroundColor='#ecebeb'
         >
-          <rect x='0' y='20' rx='8' ry='8' width='110' height='32' />
+          <rect x='0' y='0' rx='3' ry='3' width='130' height='24' />
+          <rect x='0' y='44' rx='3' ry='3' width='110' height='32' />
+          <rect x='0' y='92' rx='3' ry='3' width='100%' height='16' />
+          <rect x='0' y='115' rx='3' ry='3' width='80' height='16' />
         </ContentLoader>
-        <p className={styles.configMessage}>
-          {' '}
-          전체공개 시 내 걷기 기록이 피드 탭에서 모든 유저에게 보입니다.
-        </p>
       </div>
-      <p className={styles.setLinkWrapper900}>비밀번호 변경하기</p>
-      <p className={styles.setLinkWrapper900}>로그아웃</p>
-      <p className={styles.setLinkWrapper600}>회원 탈퇴하기</p>
+      <div className={styles.bottom}>
+        <ContentLoader
+          speed={2}
+          width='170'
+          height='200'
+          backgroundColor='#e3e3e3'
+          foregroundColor='#ecebeb'
+        >
+          <rect x='0' y='40' rx='3' ry='3' width='170' height='24' />
+          <rect x='35' y='104' rx='3' ry='3' width='100' height='24' />
+          <rect x='15' y='168' rx='3' ry='3' width='140' height='24' />
+        </ContentLoader>
+      </div>
     </div>
   )
 }
