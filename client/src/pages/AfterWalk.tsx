6aa509ddd54aee374b12002b80060eca675969ea
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-30 19:41:36 +0900

    Feat: 맵경로이미지 변환 로직 제거 (#388)
    기록리스트 페이지에서 직접 캔버스로 그릴예정

--- client/src/pages/AfterWalk.tsx
+++ client/src/pages/AfterWalk.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useAtomValue } from 'jotai'
 import useRouter from '../hooks/useRouter'
-// import useDrawPolyline from '../hooks/useDrawPolyline'
 import { getWalkLog, WalkLogType, WalkLogContentType, stopWalkLog } from '../apis/walkLog'
 import WalkHeader from '../components/header/WalkHeader'
 import styles from './AfterWalk.module.scss'
@@ -11,7 +10,6 @@ import { differenceInSeconds } from '../utils/date-fns'
 import { deleteSnap, editSnap } from '../apis/snap'
 import DropDown from '../components/common/DropDown'
 import { UserInfoAtomType, userInfoAtom } from '../store/authAtom'
-import { convertImageFromDataURL } from '../utils/imageConvertor'
 import StaticPathMap from '../components/common/Map/StaticPathMap'
 import useMapRef from '../hooks/useMapRef'
 import AfterWalkLoading from './loadingPage/AfterWalkLoading'
@@ -29,12 +27,7 @@ export default function AfterWalk() {
 
   const createdDate = walkLog && new Date(walkLog.createdAt)
 
-  // const { img, canvasRef } = useDrawPolyline(walkLog?.coordinates || [])
-
   const mapRef = useMapRef()
-
-  const [mapImg, setMapImg] = useState<string | null>(null)
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
   const getWalkLogData = async () => {
     const data = await getWalkLog(Number(walkLogId))
@@ -57,7 +50,6 @@ export default function AfterWalk() {
     event.preventDefault()
 
     if (walkLogId === undefined) return
-    if (!mapImg) return
 
     const formData = new FormData(event.currentTarget)
     const message = formData.get('message') as string
@@ -69,7 +61,6 @@ export default function AfterWalk() {
     )
 
     data.append('endPost', blob)
-    data.append('mapImage', convertImageFromDataURL(mapImg))
 
     const response = await stopWalkLog({ walkLogId, data })
     if (response) {
@@ -108,62 +99,12 @@ export default function AfterWalk() {
     getWalkLogData()
   }, [])
 
-  useEffect(() => {
-    if (!canvasRef.current) return
-
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
-
-    function drawLines() {
-      if (context && walkLog) {
-        context.clearRect(0, 0, canvas.width, canvas.height) // 캔버스 초기화
-        context.fillStyle = '#f3f4f6' // 캔버스 배경색 설정
-        context.fillRect(0, 0, canvas.width, canvas.height) // 배경색으로 사각형 그리기
-
-        const minLng = Math.min(...walkLog.coordinates.map(coord => coord.lng))
-        const maxLng = Math.max(...walkLog.coordinates.map(coord => coord.lng))
-        const minLat = Math.min(...walkLog.coordinates.map(coord => coord.lat))
-        const maxLat = Math.max(...walkLog.coordinates.map(coord => coord.lat))
-
-        const pathWidth = canvas.width * 0.6 // 경로의 가로 길이 (캔버스 너비의 60%)
-        const pathHeight = canvas.height * 0.6 // 경로의 세로 길이 (캔버스 높이의 60%)
-        const offsetX = (canvas.width - pathWidth) / 2 // X 좌표 오프셋
-        const offsetY = (canvas.height - pathHeight) / 2 // Y 좌표 오프셋
-
-        context.beginPath()
-        for (let i = 0; i < walkLog.coordinates.length; i += 1) {
-          const { lat, lng } = walkLog.coordinates[i]
-
-          // 좌표를 Canvas 좌표로 변환
-          const x = offsetX + ((lng - minLng) / (maxLng - minLng)) * pathWidth
-          const y = offsetY + ((lat - minLat) / (maxLat - minLat)) * pathHeight
-
-          if (i === 0) {
-            context.moveTo(x, y)
-          } else {
-            context.lineTo(x, y)
-          }
-        }
-        context.strokeStyle = '#8cff9e'
-        context.lineWidth = 3
-        context.stroke()
-      }
-    }
-    const polylineImg = canvas.toDataURL('image/png')
-    setMapImg(polylineImg)
-    canvas.width = 64
-    canvas.height = 64
-
-    drawLines()
-  }, [walkLog])
-
   if (walkLog === null) return <AfterWalkLoading />
 
   return (
     <div>
       <WalkHeader type='AFTER' startedAt={walkLog.createdAt} />
       <form className={styles.stopWalkForm} onSubmit={handleStopClick}>
-        <canvas ref={canvasRef} className={styles.canvasElement} />
         <div className={styles.publicOptionBox}>
           <span>기록공개 설정</span>
           <DropDown currentSetting={pubilcOption} onSubmit={updatePublicOption} />
