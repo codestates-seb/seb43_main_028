8bf15cd3ae523883980a43a235315a4599b3320e
Sangbeom Heo <sangbeomheoo@gmail.com>
2023-05-30 18:05:54 +0900

    Refactor: 맵이미지 생성 로직을 afterwalk의 useEffect 내부로 옮김 (#388)

--- client/src/pages/AfterWalk.tsx
+++ client/src/pages/AfterWalk.tsx
@@ -108,58 +108,54 @@ export default function AfterWalk() {
     getWalkLogData()
   }, [])
 
-  function drawLines(
-    canvas: HTMLCanvasElement,
-    context: CanvasRenderingContext2D,
-    coordinates: { lat: number; lng: number }[]
-  ) {
-    context.clearRect(0, 0, canvas.width, canvas.height) // 캔버스 초기화
-    context.fillStyle = '#f3f4f6' // 캔버스 배경색 설정
-    context.fillRect(0, 0, canvas.width, canvas.height) // 배경색으로 사각형 그리기
-
-    if (coordinates.length > 0) {
-      const minLng = Math.min(...coordinates.map(coord => coord.lng))
-      const maxLng = Math.max(...coordinates.map(coord => coord.lng))
-      const minLat = Math.min(...coordinates.map(coord => coord.lat))
-      const maxLat = Math.max(...coordinates.map(coord => coord.lat))
-
-      const pathWidth = canvas.width * 0.6 // 경로의 가로 길이 (캔버스 너비의 60%)
-      const pathHeight = canvas.height * 0.6 // 경로의 세로 길이 (캔버스 높이의 60%)
-      const offsetX = (canvas.width - pathWidth) / 2 // X 좌표 오프셋
-      const offsetY = (canvas.height - pathHeight) / 2 // Y 좌표 오프셋
-
-      context.beginPath()
-      for (let i = 0; i < coordinates.length; i += 1) {
-        const { lat, lng } = coordinates[i]
-
-        // 좌표를 Canvas 좌표로 변환
-        const x = offsetX + ((lng - minLng) / (maxLng - minLng)) * pathWidth
-        const y = offsetY + ((lat - minLat) / (maxLat - minLat)) * pathHeight
-
-        if (i === 0) {
-          context.moveTo(x, y)
-        } else {
-          context.lineTo(x, y)
-        }
-      }
-
-      context.strokeStyle = '#8cff9e'
-      context.lineWidth = 3
-      context.stroke()
-    }
-  }
-
   useEffect(() => {
-    if (canvasRef.current && walkLog) {
-      const canvas = canvasRef.current
-      const context = canvas.getContext('2d')
-      drawLines(canvas, context as CanvasRenderingContext2D, walkLog.coordinates)
-      canvas.width = 64
-      canvas.height = 64
-      const polylineImg = canvas.toDataURL('image/png')
-      setMapImg(polylineImg)
+    if (!canvasRef.current) return
+
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+
+    function drawLines() {
+      if (context && walkLog) {
+        context.clearRect(0, 0, canvas.width, canvas.height) // 캔버스 초기화
+        context.fillStyle = '#f3f4f6' // 캔버스 배경색 설정
+        context.fillRect(0, 0, canvas.width, canvas.height) // 배경색으로 사각형 그리기
+
+        const minLng = Math.min(...walkLog.coordinates.map(coord => coord.lng))
+        const maxLng = Math.max(...walkLog.coordinates.map(coord => coord.lng))
+        const minLat = Math.min(...walkLog.coordinates.map(coord => coord.lat))
+        const maxLat = Math.max(...walkLog.coordinates.map(coord => coord.lat))
+
+        const pathWidth = canvas.width * 0.6 // 경로의 가로 길이 (캔버스 너비의 60%)
+        const pathHeight = canvas.height * 0.6 // 경로의 세로 길이 (캔버스 높이의 60%)
+        const offsetX = (canvas.width - pathWidth) / 2 // X 좌표 오프셋
+        const offsetY = (canvas.height - pathHeight) / 2 // Y 좌표 오프셋
+
+        context.beginPath()
+        for (let i = 0; i < walkLog.coordinates.length; i += 1) {
+          const { lat, lng } = walkLog.coordinates[i]
+
+          // 좌표를 Canvas 좌표로 변환
+          const x = offsetX + ((lng - minLng) / (maxLng - minLng)) * pathWidth
+          const y = offsetY + ((lat - minLat) / (maxLat - minLat)) * pathHeight
+
+          if (i === 0) {
+            context.moveTo(x, y)
+          } else {
+            context.lineTo(x, y)
+          }
+        }
+        context.strokeStyle = '#8cff9e'
+        context.lineWidth = 3
+        context.stroke()
+      }
     }
-  }, [canvasRef, walkLog])
+    const polylineImg = canvas.toDataURL('image/png')
+    setMapImg(polylineImg)
+    canvas.width = 64
+    canvas.height = 64
+
+    drawLines()
+  }, [walkLog])
 
   if (walkLog === null) return <AfterWalkLoading />
 
